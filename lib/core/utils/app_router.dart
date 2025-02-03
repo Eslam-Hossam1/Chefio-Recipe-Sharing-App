f55aa10bc72777ade77b56504ac62ee494846723
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-03 05:46:05 +0200

    add app router file and modify main file to add easy localization , router , screen util and device preview

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,12 @@
+// GoRouter configuration
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  
+  
+  static final router = GoRouter(
+    routes: [
+    ],
+  );
+}
