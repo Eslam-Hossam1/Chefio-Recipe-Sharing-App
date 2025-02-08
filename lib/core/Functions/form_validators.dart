4e41f9e514f90b7211507085484ba0a140e23930
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-08 09:24:40 +0200

    add form validators class, move custom text form field to form validators class

---
+++ lib/core/Functions/form_validators.dart
@@ -0,0 +1,57 @@
+import 'package:chefio_app/core/utils/app_localization_keys.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+abstract class FormValidators {
+  static  String? customTextFormFieldValidator(String? value) {
+    if (isValueEmpty(value)) {
+      return AppLocalizationKeys.auth.thisFieldRequired.tr();
+    } else {
+      return null;
+    }
+    
+  }
+  
+
+  static String? usernameTextFormFieldValidator(String? value) {
+     final RegExp usernameRegex = RegExp(r'^[a-zA-Z0-9_]+$');
+    if (isValueEmpty(value)) {
+      return AppLocalizationKeys.auth.thisFieldRequired.tr();
+    } else if(value!.contains(' ')) {
+      return AppLocalizationKeys.auth.signUpViewUsernameCantContainSpaces.tr();
+    }else if (value.length < 2|| value.length > 20) {
+      return AppLocalizationKeys.auth.signUpViewUserNameMustBeBetween.tr();
+    } else if (!usernameRegex.hasMatch(value)) {
+    return AppLocalizationKeys.auth.signUpViewUsernameCanOnlyContain.tr();
+    }else{
+      return null;
+    }
+    
+  }
+  
+  static String? emailTextFormFieldValidator(String? value) {
+    if (isValueEmpty(value)) {
+      return AppLocalizationKeys.auth.thisFieldRequired.tr();
+    } else if (!isValidEmail(value!)) {
+      return AppLocalizationKeys.auth.invalidEmail.tr();
+    } else {
+      return null;
+    }
+
+  }
+
+  static bool isValueEmpty(String? value){
+    if(value == null || value.isEmpty){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  static bool isValidEmail(String email) {
+  final RegExp emailRegex = RegExp(
+    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+  );
+  return emailRegex.hasMatch(email);
+}
+
+}
--- lib/core/Functions/text_form_field_validator.dart
+++ lib/core/Functions/text_form_field_validator.dart
@@ -1,10 +1,4 @@
 import 'package:chefio_app/core/utils/app_localization_keys.dart';
 import 'package:easy_localization/easy_localization.dart';
 
-String? customTextFormFieldValidator(String? value) {
-  if (value == null || value.isEmpty) {
-    return AppLocalizationKeys.auth.thisFieldRequired.tr();
-  } else {
-    return null;
-  }
-}
+
