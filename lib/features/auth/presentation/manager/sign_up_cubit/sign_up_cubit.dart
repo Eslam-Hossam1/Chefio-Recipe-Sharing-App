3371ba16732af3cc4d0c9dfeb9f28ab03087c4ad
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-08 06:58:40 +0200

    feat(auth) : add sign up cubit

---
+++ lib/features/auth/presentation/manager/sign_up_cubit/sign_up_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:chefio_app/features/auth/data/repos/auth_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'sign_up_state.dart';
+
+class SignUpCubit extends Cubit<SignUpState> {
+  SignUpCubit(this._authRepo) : super(SignUpInitial());
+  final AuthRepo _authRepo;
+  Future<void> signUp(
+      {required String username,
+      required String email,
+      required String password}) async {
+    emit(SignUpLoading());
+
+    var signUpResult = await _authRepo.signUp(
+        username: username, email: email, password: password);
+
+    signUpResult.fold(
+      (failure) => emit(SignUpFailure(errorMessage: failure.errMsg)),
+      (signUpSuccess) => SignUpSuccess(),
+    );
+  }
+}
