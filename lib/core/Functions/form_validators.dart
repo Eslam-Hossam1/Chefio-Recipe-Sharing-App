34fe9a36e152a0c9001e98723e12878c8af95393
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-08 11:46:18 +0200

    update form validator : modify isValidEmail method& update app localization

--- lib/core/Functions/form_validators.dart
+++ lib/core/Functions/form_validators.dart
@@ -49,7 +49,7 @@ abstract class FormValidators {
 
   static bool isValidEmail(String email) {
   final RegExp emailRegex = RegExp(
-    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+     r"^(?!.*\.\.)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
   );
   return emailRegex.hasMatch(email);
 }
--- lib/core/Functions/text_form_field_validator.dart
+++
@@ -1,4 +0,0 @@
-import 'package:chefio_app/core/utils/app_localization_keys.dart';
-import 'package:easy_localization/easy_localization.dart';
-
-
