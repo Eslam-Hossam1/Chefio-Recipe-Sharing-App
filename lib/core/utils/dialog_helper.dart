3f40aa412193b0b83d6e2c2786849b1eb2d5e30f
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-08 07:32:41 +0200

    update dialog helper class

--- lib/core/utils/dialog_helper.dart
+++ lib/core/utils/dialog_helper.dart
@@ -4,26 +4,26 @@ import 'package:chefio_app/core/utils/app_localization_keys.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-class DialogHelper {
-  static void showErrorDialog(BuildContext context, String errorMessage) {
+abstract class DialogHelper {
+  static void showErrorDialog(BuildContext context,{required String errorMessage,void Function()? btnOkOnPress, void Function(DismissType)? onDismissCallback}) {
     customAdaptiveAwesomeDialog(
       context,
       dialogType: DialogType.error,
       title: AppLocalizationKeys.global.error.tr(),
       desc: errorMessage,
-      btnOkOnPress: () {},
+      btnOkOnPress:btnOkOnPress,
+      onDismissCallback: onDismissCallback,
     ).show();
   }
 
-  static void showSuccessDialog(BuildContext context, String successMessage,
-      VoidCallback onDismissCallback) {
+  static void showSuccessDialog(BuildContext context,{required String successMessage,void Function()? btnOkOnPress, void Function(DismissType)? onDismissCallback}) {
     customAdaptiveAwesomeDialog(
       context,
       dialogType: DialogType.success,
       title: AppLocalizationKeys.global.success.tr(),
       desc: successMessage,
-      btnOkOnPress: () {},
-      onDismissCallback: (_) => onDismissCallback(),
+      btnOkOnPress: btnOkOnPress,
+      onDismissCallback:onDismissCallback,
     ).show();
   }
 }
