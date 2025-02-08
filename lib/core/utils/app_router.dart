5d76da400431b1e57e4883b08e540d89b7c133fe
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-08 09:33:15 +0200

    provide validate sign up password cubit

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -2,6 +2,7 @@
 import 'package:chefio_app/core/utils/service_locator.dart';
 import 'package:chefio_app/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:chefio_app/features/auth/presentation/manager/sign_up_cubit/sign_up_cubit.dart';
+import 'package:chefio_app/features/auth/presentation/manager/validate_sign_up_password_cubit/validate_sign_up_password_cubit.dart';
 import 'package:chefio_app/features/auth/presentation/view/login_view.dart';
 import 'package:chefio_app/features/auth/presentation/view/reset_password_view.dart';
 import 'package:chefio_app/features/auth/presentation/view/sign_up_view.dart';
@@ -49,8 +50,15 @@ abstract class AppRouter {
       ),
       GoRoute(
         path: kSignUpView,
-        builder: (context, state) => BlocProvider(
-          create: (context) => SignUpCubit(getIt<AuthRepoImpl>()),
+        builder: (context, state) => MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => SignUpCubit(getIt<AuthRepoImpl>()),
+            ),
+            BlocProvider(
+              create: (context) => ValidateSignUpPasswordCubit(),
+            ),
+          ],
           child: const SignUpView(),
         ),
       ),
