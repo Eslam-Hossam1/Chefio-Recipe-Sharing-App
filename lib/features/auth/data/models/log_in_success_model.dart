cf18d6e26938dfd5da69dfcc7e5712be26a998aa
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-10 05:52:16 +0200

    add login success model

---
+++ lib/features/auth/data/models/log_in_success_model.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+
+class LogInSuccessModel extends Equatable {
+  final bool? success;
+  final String? token;
+  final String? message;
+
+  const LogInSuccessModel({this.success, this.token, this.message});
+
+  factory LogInSuccessModel.fromJson(Map<String, dynamic> json) {
+    return LogInSuccessModel(
+      success: json['success'] as bool?,
+      token: json['token'] as String,
+      message: json['message'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'success': success,
+        'token': token,
+        'message': message,
+      };
+
+  @override
+  List<Object?> get props => [success, token, message];
+}
