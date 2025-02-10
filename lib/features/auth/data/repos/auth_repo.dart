b64c78abd1a437bb6f604b4dd9f5a0e332710e19
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-10 05:53:53 +0200

    feat(auth) : add login method to auth repo

--- lib/features/auth/data/repos/auth_repo.dart
+++ lib/features/auth/data/repos/auth_repo.dart
@@ -1,15 +1,24 @@
 import 'package:chefio_app/core/errors/failures.dart';
+import 'package:chefio_app/features/auth/data/models/log_in_success_model.dart';
 import 'package:chefio_app/features/auth/data/models/sign_up_success_model/sign_up_success_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class AuthRepo {
-  Future<Either<Failure, SignUpSuccessModel>> signUp(
-      {required String username,
-      required String email,
-      required String password});
+  Future<Either<Failure, SignUpSuccessModel>> signUp({
+    required String username,
+    required String email,
+    required String password,
+  });
+
+  Future<Either<Failure, LogInSuccessModel>> logIn({
+    required String email,
+    required String password,
+  });
 
   Future<Either<Failure, void>> sendVerificationCode({required String email});
 
-  Future<Either<Failure, void>> verifyVerificationCode(
-      {required String email, required int code});
+  Future<Either<Failure, void>> verifyVerificationCode({
+    required String email,
+    required int code,
+  });
 }
