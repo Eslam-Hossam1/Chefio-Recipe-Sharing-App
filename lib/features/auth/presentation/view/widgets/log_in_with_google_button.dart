2969fa505ca0f6a4d0f98480089bef3f2e08c86a
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-10 07:00:40 +0200

    integrate login cubit with login buttons

--- lib/features/auth/presentation/view/widgets/log_in_with_google_button.dart
+++ lib/features/auth/presentation/view/widgets/log_in_with_google_button.dart
@@ -1,10 +1,16 @@
+import 'package:chefio_app/core/utils/app_router.dart';
 import 'package:chefio_app/core/utils/assets.dart';
+import 'package:chefio_app/core/utils/dialog_helper.dart';
 import 'package:chefio_app/core/utils/styles.dart';
+import 'package:chefio_app/core/widgets/custom_cicular_progress_indicator.dart';
 import 'package:chefio_app/core/widgets/custom_text_button.dart';
+import 'package:chefio_app/features/auth/presentation/manager/log_in_cubit.dart/log_in_cubit.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
 
 class LogInWithGoogleButton extends StatelessWidget {
   const LogInWithGoogleButton({
@@ -13,28 +19,54 @@ class LogInWithGoogleButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: CustomTextButton(
-            backgroundColor: const Color(0xffFF5842),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(Assets.imagesGoogleIcon),
-                SizedBox(
-                  width: 4,
+    return BlocConsumer<LogInCubit, LogInState>(
+      listener: (context, state) {
+        if (state is LogInFailure) {
+          DialogHelper.showErrorDialog(
+            context,
+            errorMessage: state.errorMessage,
+            btnOkOnPress: () {},
+          );
+        } else if (state is LogInSuccess) {
+          context.go(AppRouter.kHomeView);
+        }
+      },
+      builder: (context, state) {
+        bool isLoading = (state is LogInLoadingWithEmailAndPassword) ||
+            (state is LogInLoadingWithGoogle);
+        bool showLoadingIndicator = (state is LogInLoadingWithGoogle);
+        return Row(
+          children: [
+            Expanded(
+              child: AbsorbPointer(
+                absorbing: showLoadingIndicator,
+                child: CustomTextButton(
+                  backgroundColor: const Color(0xffFF5842),
+                  child: isLoading
+                      ? Center(
+                          child: CustomCircularProgressIndicator(
+                          color: Colors.white,
+                        ))
+                      : Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(Assets.imagesGoogleIcon),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Text(
+                              "Google",
+                              style: Styles.textStyleBold15(context)
+                                  .copyWith(color: Colors.white),
+                            ),
+                          ],
+                        ),
                 ),
-                Text(
-                  "Google",
-                  style: Styles.textStyleBold15(context)
-                      .copyWith(color: Colors.white),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ],
+          ],
+        );
+      },
     );
   }
 }
--- lib/features/auth/presentation/view/widgets/login_with_email_and_password_button.dart
+++ lib/features/auth/presentation/view/widgets/login_with_email_and_password_button.dart
@@ -1,8 +1,14 @@
 import 'package:chefio_app/core/utils/app_localization_keys.dart';
+import 'package:chefio_app/core/utils/app_router.dart';
+import 'package:chefio_app/core/utils/dialog_helper.dart';
 import 'package:chefio_app/core/utils/styles.dart';
+import 'package:chefio_app/core/widgets/custom_cicular_progress_indicator.dart';
 import 'package:chefio_app/core/widgets/custom_text_button.dart';
+import 'package:chefio_app/features/auth/presentation/manager/log_in_cubit.dart/log_in_cubit.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class LoginWithEmailAndPasswordButton extends StatelessWidget {
   const LoginWithEmailAndPasswordButton({
@@ -12,19 +18,46 @@ class LoginWithEmailAndPasswordButton extends StatelessWidget {
   final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: CustomTextButton(
-            onPressed: onPressed,
-            child: Text(
-              AppLocalizationKeys.auth.logIn.tr(),
-              style:
-                  Styles.textStyleBold15(context).copyWith(color: Colors.white),
+    return BlocConsumer<LogInCubit, LogInState>(
+      listener: (context, state) {
+        if (state is LogInFailure) {
+          DialogHelper.showErrorDialog(
+            context,
+            errorMessage: state.errorMessage,
+            btnOkOnPress: () {},
+          );
+        } else if (state is LogInNeedVerification) {
+          context.go(AppRouter.kVerificationCodeView, extra: state.email);
+        } else if (state is LogInSuccess) {
+          context.go(AppRouter.kHomeView);
+        }
+      },
+      builder: (context, state) {
+        bool isLoading = (state is LogInLoadingWithEmailAndPassword)||(state is LogInLoadingWithGoogle);
+        bool showLoadingIndicator = (state is LogInLoadingWithEmailAndPassword);
+        return Row(
+          children: [
+            Expanded(
+              child: AbsorbPointer(
+                absorbing: isLoading,
+                child: CustomTextButton(
+                  onPressed: onPressed,
+                  child: showLoadingIndicator
+                      ? Center(
+                          child: CustomCircularProgressIndicator(
+                          color: Colors.white,
+                        ))
+                      : Text(
+                          AppLocalizationKeys.auth.logIn.tr(),
+                          style: Styles.textStyleBold15(context)
+                              .copyWith(color: Colors.white),
+                        ),
+                ),
+              ),
             ),
-          ),
-        ),
-      ],
+          ],
+        );
+      },
     );
   }
 }
