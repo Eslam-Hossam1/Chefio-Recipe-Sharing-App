faf8cc4ff4c19a7904a4e77c4e1d2a426de47d24
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-12 03:45:43 +0200

    create/integrate/triger validate reset password cubit

---
+++ lib/features/auth/presentation/manager/validate_reset_password/validate_reset_password_state.dart
@@ -0,0 +1,12 @@
+part of 'validate_reset_password_cubit.dart';
+
+sealed class ValidateResetPasswordState extends Equatable {
+  const ValidateResetPasswordState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class ValidateResetPasswordInitial extends ValidateResetPasswordState {}
+final class ValidateResetPasswordUpdate extends ValidateResetPasswordState {}
+
