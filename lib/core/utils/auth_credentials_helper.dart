6cfef2a7f705220101fd4b74d29cfdd2bbdd2eaf
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-22 09:43:48 +0200

    create auth credentials helper class

---
+++ lib/core/utils/auth_credentials_helper.dart
@@ -0,0 +1,63 @@
+import 'package:chefio_app/core/api/api_keys.dart';
+import 'package:chefio_app/core/utils/secure_storage_helper.dart';
+import 'package:chefio_app/core/utils/service_locator.dart';
+
+class AuthCredentialsHelper {
+  final SecureStorageHelper secureStorageHelper;
+
+  AuthCredentialsHelper({required this.secureStorageHelper});
+
+  AuthCredentials? _authCredentialsModel;
+
+  Future<void> init() async {
+    _authCredentialsModel = AuthCredentials(
+      accessToken: await getAccessToken(),
+      refreshToken: await getRefreshToken(),
+    );
+  }
+
+  String? get accessToken => _authCredentialsModel?.accessToken;
+  String? get refreshToken => _authCredentialsModel?.refreshToken;
+
+  Future<String?> getAccessToken() async {
+    return await secureStorageHelper.getString(key: ApiKeys.accessToken);
+  }
+
+  Future<String?> getRefreshToken() async {
+    return await secureStorageHelper.getString(key: ApiKeys.refreshToken);
+  }
+
+  Future<void> storeAccessToken(String token) async {
+    await secureStorageHelper.setData(key: ApiKeys.accessToken, value: token);
+    _authCredentialsModel?.accessToken = token;
+  }
+
+  Future<void> storeRefreshToken(String token) async {
+    await secureStorageHelper.setData(key: ApiKeys.refreshToken, value: token);
+    _authCredentialsModel?.refreshToken = token;
+  }
+
+  Future<void> storeTokens({required String accessToken,required String refreshToken}) async {
+    await Future.wait([
+      storeAccessToken(accessToken),
+      storeRefreshToken(refreshToken),
+    ]);
+  }
+
+  Future<void> clearTokens() async {
+    await secureStorageHelper.clear();
+    _authCredentialsModel = null;
+  }
+
+  bool userIsAuthenticated() {
+    bool isLoggedIn = _authCredentialsModel?.accessToken != null;
+    return isLoggedIn;
+  }
+}
+
+class AuthCredentials {
+  String? accessToken;
+  String? refreshToken;
+
+  AuthCredentials({required this.accessToken, required this.refreshToken});
+}
