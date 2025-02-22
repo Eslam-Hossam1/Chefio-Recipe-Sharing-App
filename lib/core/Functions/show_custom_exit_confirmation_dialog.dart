288fc47bbe59bd9aa5e5c50517f97c906380c84b
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-22 09:38:15 +0200

    modify show custom exit confirmation dialog

--- lib/core/Functions/show_custom_exit_confirmation_dialog.dart
+++ lib/core/Functions/show_custom_exit_confirmation_dialog.dart
@@ -1,17 +1,18 @@
- import 'package:chefio_app/core/utils/app_localization_keys.dart';
+import 'package:chefio_app/core/utils/app_localization_keys.dart';
+import 'package:chefio_app/core/utils/app_router.dart';
 import 'package:chefio_app/core/utils/dialog_helper.dart';
 import 'package:chefio_app/core/utils/theme_colors_extension.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-void showCustomExitConfirmationDialog(BuildContext context,{required String errorMessage}) {
-     DialogHelper.showWarningDialog(context,
-        errorMessage: errorMessage,
-            btnOkOnPress: () => context.pop(),
-            btnOkColor:context.secondaryColor,
-            btnOkText: AppLocalizationKeys.global.exit.tr(),
-            btnCancelColor:context.primaryColor,
-            btnCanceltext: AppLocalizationKeys.global.stay.tr()
-            );
-  }
+void showCustomExitConfirmationDialog(BuildContext context,
+    {required String errorMessage,required void Function()? btnOkOnPress}) {
+  DialogHelper.showWarningDialog(context,
+      errorMessage: errorMessage,
+      btnOkOnPress: btnOkOnPress,
+      btnOkColor: context.secondaryColor,
+      btnOkText: AppLocalizationKeys.global.exit.tr(),
+      btnCancelColor: context.primaryColor,
+      btnCanceltext: AppLocalizationKeys.global.stay.tr());
+}
