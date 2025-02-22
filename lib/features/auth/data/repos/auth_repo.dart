9c39f2fd93ce6e427450086aad0ce9b72c9b0969
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-22 09:42:52 +0200

    modify verfify verfication code and reset password methods in auth repo

--- lib/features/auth/data/repos/auth_repo.dart
+++ lib/features/auth/data/repos/auth_repo.dart
@@ -1,3 +1,4 @@
+import 'package:chefio_app/core/errors/api_failure.dart';
 import 'package:chefio_app/core/errors/failures.dart';
 import 'package:chefio_app/features/auth/data/models/log_in_success_model.dart';
 import 'package:chefio_app/features/auth/data/models/sign_up_success_model/sign_up_success_model.dart';
@@ -5,29 +6,36 @@ import 'package:dartz/dartz.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthRepo {
-  Future<Either<Failure, SignUpSuccessModel>> signUp({
+  Future<Either<ApiFailure, SignUpSuccessModel>> signUp({
     required String username,
     required String email,
     required String password,
   });
 
-  Future<Either<Failure, LogInSuccessModel>> logIn({
+  Future<Either<ApiFailure, LogInSuccessModel>> logIn({
     required String email,
     required String password,
   });
 
-  Future<Either<Failure, void>> sendVerificationCode({required String email});
+  Future<Either<ApiFailure, void>> sendVerificationCode(
+      {required String email});
 
-  Future<Either<Failure, void>> verifyVerificationCode({
+  Future<Either<ApiFailure, void>> verifyVerificationCode({
     required String email,
     required int code,
   });
-  Future<Either<Failure, void>> enterEmailToRecoverPassword({
+
+  Future<Either<ApiFailure, void>> verifyForgotPasswordVerificationCode({
+    required String email,
+    required int code,
+  });
+  Future<Either<ApiFailure, void>> sendForgotPasswordVerificationCode({
     required String email,
   });
-  Future<Either<Failure, void>> resetPassword({
-    required String password,
+  Future<Either<ApiFailure, void>> resetPassword({
+    required String email,
+    required String newPassword,
   });
 
-  Future<Either<Failure, LogInSuccessModel?>> logInWithGoogle();
+  Future<Either<ApiFailure, LogInSuccessModel?>> logInWithGoogle();
 }
