c0dd73deb4031a752068dad5c5956f4f7e3ac279
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-22 09:45:27 +0200

    init auth credential helper class in main & refactor main and create chefio app widget

---
+++ lib/chefio_app.dart
@@ -0,0 +1,48 @@
+
+import 'package:chefio_app/core/Functions/get_text_theme.dart';
+import 'package:chefio_app/core/cubit/theme_cubit/theme_cubit.dart';
+import 'package:chefio_app/core/utils/app_router.dart';
+import 'package:chefio_app/core/utils/app_themes.dart';
+import 'package:chefio_app/core/utils/constants.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Chefio extends StatelessWidget {
+  const Chefio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(Constants.kDesignWidth, Constants.kDesignHeight),
+        minTextAdapt: true,
+        builder: (context, child) {
+          return BlocProvider(
+            create: (context) => ThemeCubit(),
+            child: BlocBuilder<ThemeCubit, ThemeMode>(
+              builder: (context, mode) {
+                return MaterialApp.router(
+                  routerConfig: AppRouter.router,
+                  locale: context.locale,
+                  supportedLocales: context.supportedLocales,
+                  localizationsDelegates: context.localizationDelegates,
+                  builder: DevicePreview.appBuilder,
+                  //getting textTheme bassed on context cuz of using easy localization
+                  // for localization to access current locale and get the textTheme bassed on it
+                  themeMode: ThemeMode.dark,
+                  theme: AppThemes.lightTheme
+                      .copyWith(textTheme: getTextTheme(context)),
+                  darkTheme: AppThemes.darkTheme
+                      .copyWith(textTheme: getTextTheme(context)),
+
+                  debugShowCheckedModeBanner: false,
+                );
+              },
+            ),
+          );
+        });
+  }
+}
