9e190465e683513e10939c0a74980e5ddc211865
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-24 02:52:51 +0200

    modify search button

---
+++ lib/features/home/presentation/view/widgets/search_button.dart
@@ -0,0 +1,27 @@
+import 'package:chefio_app/core/utils/app_localization_keys.dart';
+import 'package:chefio_app/core/utils/assets.dart';
+import 'package:chefio_app/core/utils/constants.dart';
+import 'package:chefio_app/core/utils/styles.dart';
+import 'package:chefio_app/core/utils/theme_colors_extension.dart';
+import 'package:chefio_app/core/widgets/custom_text_button.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SearchButton extends StatelessWidget {
+  const SearchButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {},
+      icon: SvgPicture.asset(
+        Assets.imagesSearch,
+        colorFilter: ColorFilter.mode(context.mainTextColor, BlendMode.srcIn),
+      ),
+    );
+  }
+}
