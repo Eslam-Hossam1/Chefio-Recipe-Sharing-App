5041de8f04d15797917e9b013a903c85c72ccb73
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-24 06:28:07 +0200

    create sliver adaptive padding

---
+++ lib/core/widgets/sliver_adaptive_padding.dart
@@ -0,0 +1,23 @@
+import 'package:chefio_app/core/utils/constants.dart';
+import 'package:chefio_app/core/utils/size_config.dart';
+import 'package:flutter/material.dart';
+
+class SliverAdaptivePadding extends StatelessWidget {
+  const SliverAdaptivePadding({super.key, required this.sliver});
+    final Widget sliver;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPadding(padding: EdgeInsets.symmetric(horizontal: getPadding(context)),
+        sliver: sliver,);
+  }
+
+  double getPadding(BuildContext context) {
+    double width = MediaQuery.sizeOf(context).width;
+    if (width < SizeConfig.tabletBreakPoint) {
+      return Constants.kMobileHorizontalPadding;
+    } else {
+      return Constants.kTabletHorizontalpadding;
+    }
+  }
+}
