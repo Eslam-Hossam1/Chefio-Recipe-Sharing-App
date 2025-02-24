2166bda1ee0ab0763072223aa75690b6e07625af
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-02-24 02:53:06 +0200

    create adaptive padding widget

---
+++ lib/core/widgets/adaptive_padding.dart
@@ -0,0 +1,23 @@
+import 'package:chefio_app/core/utils/constants.dart';
+import 'package:chefio_app/core/utils/size_config.dart';
+import 'package:flutter/material.dart';
+
+class AdaptivePadding extends StatelessWidget {
+  const AdaptivePadding({super.key, required this.child});
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: getPadding(context)),
+        child: child,);
+  }
+
+  double getPadding(BuildContext context) {
+    double width = MediaQuery.sizeOf(context).width;
+    if (width < SizeConfig.tabletBreakPoint) {
+      return Constants.kMobileHorizontalPadding;
+    } else {
+      return Constants.kTabletHorizontalpadding;
+    }
+  }
+}
