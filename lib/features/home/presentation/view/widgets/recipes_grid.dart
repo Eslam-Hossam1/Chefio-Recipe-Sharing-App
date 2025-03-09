09d1e31566c8ad4d4aa3ddd2bcde7127c480c625
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-09 15:02:17 +0200

    apply changes to recipes grid to make it fit for different layouts

--- lib/features/home/presentation/view/widgets/recipes_grid.dart
+++ lib/features/home/presentation/view/widgets/recipes_grid.dart
@@ -1,15 +1,20 @@
+import 'package:chefio_app/core/utils/constants.dart';
+import 'package:chefio_app/core/utils/size_config.dart';
 import 'package:chefio_app/features/home/presentation/view/widgets/recipe_item.dart';
 import 'package:flutter/material.dart';
 
 class RecipesGrid extends StatelessWidget {
-  const RecipesGrid({super.key,  this.crossAxisCount});
-  final int? crossAxisCount;
+  const RecipesGrid({
+    super.key,
+  });
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.sizeOf(context).width;
+    bool isMobile = width < SizeConfig.tabletBreakPoint;
     return SliverGrid.builder(
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: crossAxisCount??2,
-          childAspectRatio: 0.571,
+          crossAxisCount: isMobile ? 2 : 3,
+          childAspectRatio: isMobile ? 0.500 : 0.560,
           crossAxisSpacing: 25,
           mainAxisSpacing: 32),
       itemBuilder: (context, index) {
