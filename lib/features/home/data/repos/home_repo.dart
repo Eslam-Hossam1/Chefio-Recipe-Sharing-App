fe9a7d14a499d5e0f425d4a6408f6e938899f31a
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-10 15:28:26 +0200

    create home repo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:chefio_app/core/errors/api_failure.dart';
+import 'package:chefio_app/core/errors/failures.dart';
+import 'package:chefio_app/features/home/data/models/recipe_model/recipe_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<ApiFailure,List<RecipeModel>>> fetchRecipesFromApi({required String name,required int limit,required int skips});
+}
