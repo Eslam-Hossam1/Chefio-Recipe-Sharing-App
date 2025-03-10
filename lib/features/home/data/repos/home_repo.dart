39e8d8a4b869fd0dc42e96c8646a7463da3864ea
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-10 15:48:15 +0200

    create home repo impl

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -4,5 +4,6 @@ import 'package:chefio_app/features/home/data/models/recipe_model/recipe_model.d
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<ApiFailure,List<RecipeModel>>> fetchRecipesFromApi({required String name,required int limit,required int skips});
+  Future<Either<ApiFailure, List<RecipeModel>>> fetchRecipesFromApi(
+      {required String name, required int limit, required int skip});
 }
---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,39 @@
+import 'package:chefio_app/core/api/api_consumer.dart';
+import 'package:chefio_app/core/api/api_keys.dart';
+import 'package:chefio_app/core/api/dio_consumer.dart';
+import 'package:chefio_app/core/api/end_ponits.dart';
+import 'package:chefio_app/core/errors/api_failure.dart';
+import 'package:chefio_app/core/errors/dio_api_failure.dart';
+import 'package:chefio_app/features/home/data/models/recipe_model/recipe_model.dart';
+import 'package:chefio_app/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  final ApiConsumer _apiConsumer;
+  HomeRepoImpl(this._apiConsumer);
+  @override
+  Future<Either<ApiFailure, List<RecipeModel>>> fetchRecipesFromApi(
+      {required String name, required int limit, required int skip}) async {
+    try {
+      final response = await _apiConsumer.get(EndPoints.home, queryParameters: {
+        ApiKeys.limit: limit,
+        ApiKeys.skip: skip,
+      });
+      List<Map<String, dynamic>> products = response['products'];
+      final List<RecipeModel> recipes = products
+          .map(
+            (recipeJson) => RecipeModel.fromJson(recipeJson),
+          )
+          .toList();
+
+      return Right(recipes);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(DioApiFailure.fromDioException(e));
+      } else {
+        return Left(DioApiFailure.unknown(e.toString()));
+      }
+    }
+  }
+}
