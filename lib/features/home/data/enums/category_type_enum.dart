84b51801f3496b181ee796663b1f01230511ff07
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-10 15:10:29 +0200

    remove unused category type extension

--- lib/features/home/data/enums/category_type_enum.dart
+++ lib/features/home/data/enums/category_type_enum.dart
@@ -1,14 +1,5 @@
-enum CategoryType { all, food, drink }
-
-extension CategoryTypeExtension on CategoryType {
-  String get name {
-    switch (this) {
-      case CategoryType.all:
-        return "ALL";
-      case CategoryType.food:
-        return "Food";
-      case CategoryType.drink:
-        return "Drink";
-    }
-  }
+enum CategoryType {
+  all,
+  food,
+  drink,
 }
