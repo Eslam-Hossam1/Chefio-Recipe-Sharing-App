1e5338221e5ab23311f907c24bd9f8d57eb50511
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-11 04:17:40 +0200

    register home repo impl

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -5,6 +5,7 @@ import 'package:chefio_app/core/utils/secure_storage_helper.dart';
 import 'package:chefio_app/core/utils/shared_prefernce_helper.dart';
 import 'package:chefio_app/features/auth/data/repos/auth_repo.dart';
 import 'package:chefio_app/features/auth/data/repos/auth_repo_impl.dart';
+import 'package:chefio_app/features/home/data/repos/home_repo_impl.dart';
 import 'package:chefio_app/features/onboarding/presentation/view_model/onboarding_view_model.dart';
 import 'package:chefio_app/features/splash/presentation/view_model/splash_view_model.dart';
 import 'package:dio/dio.dart';
@@ -20,25 +21,27 @@ Future<void> setupServiceLocator() async {
   AndroidOptions getAndroidOptions() => const AndroidOptions(
         encryptedSharedPreferences: true,
       );
-  final secureStorage = FlutterSecureStorage(aOptions: getAndroidOptions());
+  final secureStorage = FlutterSecureStorage(aOptions: getAndroidOptions(),);
 
   getIt.registerSingleton<SharedPreferencesHelper>(
     SharedPreferencesHelper(sharedPreferences),
   );
 
   getIt.registerSingleton<SecureStorageHelper>(
-      SecureStorageHelper(secureStorage));
+      SecureStorageHelper(secureStorage),);
 
-  getIt.registerLazySingleton<OnboardingViewModel>(() => OnboardingViewModel());
+  getIt.registerLazySingleton<OnboardingViewModel>(() => OnboardingViewModel(),);
 
   getIt.registerSingleton<AuthCredentialsHelper>(
-      AuthCredentialsHelper(secureStorageHelper: getIt<SecureStorageHelper>()));
+      AuthCredentialsHelper(secureStorageHelper: getIt<SecureStorageHelper>(),),);
 
-  getIt.registerSingleton<DioConsumer>(DioConsumer(dio: Dio()));
+  getIt.registerSingleton<DioConsumer>(DioConsumer(dio: Dio(),),);
 
-  getIt.registerSingleton<GoogleAuthService>(GoogleAuthService());
+  getIt.registerSingleton<GoogleAuthService>(GoogleAuthService(),);
   getIt.registerSingleton<SplashViewModel>(
-      SplashViewModel(getIt<AuthCredentialsHelper>()));
+      SplashViewModel(getIt<AuthCredentialsHelper>(),),);
   getIt.registerSingleton<AuthRepoImpl>(
-      AuthRepoImpl(getIt<DioConsumer>(), getIt<GoogleAuthService>()));
+      AuthRepoImpl(getIt<DioConsumer>(), getIt<GoogleAuthService>(),),);
+  getIt.registerSingleton<HomeRepoImpl>(
+      HomeRepoImpl(getIt<DioConsumer>()),);
 }
