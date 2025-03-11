879f694d20ace61a6e6741a865b270977781f2a5
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-11 04:36:17 +0200

    add category type extension

--- lib/features/home/data/enums/category_type_enum.dart
+++ lib/features/home/data/enums/category_type_enum.dart
@@ -3,3 +3,16 @@ enum CategoryType {
   food,
   drink,
 }
+
+extension CategoryTypeExtension on CategoryType {
+  String get name {
+    switch (this) {
+      case CategoryType.all:
+        return "aLL";
+      case CategoryType.food:
+        return "food";
+      case CategoryType.drink:
+        return "drink";
+    }
+  }
+}
