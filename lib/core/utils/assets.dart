cd3a28309f2a2df163a8b12f9328b5d490e25f19
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-14 16:38:13 +0200

    replace profile.png with profile.svg

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,11 +1,10 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-
+  
   /// Assets for imagesAddCoverPhotoIcon
   /// assets/images/add_cover_photo_icon.svg
-  static const String imagesAddCoverPhotoIcon =
-      "assets/images/add_cover_photo_icon.svg";
+  static const String imagesAddCoverPhotoIcon = "assets/images/add_cover_photo_icon.svg";
 
   /// Assets for imagesArrowUpward
   /// assets/images/arrow_upward.svg
@@ -13,8 +12,7 @@ class Assets {
 
   /// Assets for imagesBigUserAvatarImageTest
   /// assets/images/big_user_avatar_image_test.png
-  static const String imagesBigUserAvatarImageTest =
-      "assets/images/big_user_avatar_image_test.png";
+  static const String imagesBigUserAvatarImageTest = "assets/images/big_user_avatar_image_test.png";
 
   /// Assets for imagesBottomDish
   /// assets/images/bottom_dish.png
@@ -22,13 +20,11 @@ class Assets {
 
   /// Assets for imagesBottomLeftDish
   /// assets/images/bottom_left_dish.png
-  static const String imagesBottomLeftDish =
-      "assets/images/bottom_left_dish.png";
+  static const String imagesBottomLeftDish = "assets/images/bottom_left_dish.png";
 
   /// Assets for imagesBottomRightDish
   /// assets/images/bottom_right_dish.png
-  static const String imagesBottomRightDish =
-      "assets/images/bottom_right_dish.png";
+  static const String imagesBottomRightDish = "assets/images/bottom_right_dish.png";
 
   /// Assets for imagesCamera
   /// assets/images/camera.svg
@@ -40,13 +36,11 @@ class Assets {
 
   /// Assets for imagesCenterLeftDish
   /// assets/images/center_left_dish.png
-  static const String imagesCenterLeftDish =
-      "assets/images/center_left_dish.png";
+  static const String imagesCenterLeftDish = "assets/images/center_left_dish.png";
 
   /// Assets for imagesCenterRightDish
   /// assets/images/center_right_dish.png
-  static const String imagesCenterRightDish =
-      "assets/images/center_right_dish.png";
+  static const String imagesCenterRightDish = "assets/images/center_right_dish.png";
 
   /// Assets for imagesCloseIcon
   /// assets/images/close_icon.svg
@@ -74,8 +68,7 @@ class Assets {
 
   /// Assets for imagesFoodDetailsPictureTest
   /// assets/images/food_details_picture_test.png
-  static const String imagesFoodDetailsPictureTest =
-      "assets/images/food_details_picture_test.png";
+  static const String imagesFoodDetailsPictureTest = "assets/images/food_details_picture_test.png";
 
   /// Assets for imagesGoogleIcon
   /// assets/images/google_icon.svg
@@ -99,8 +92,7 @@ class Assets {
 
   /// Assets for imagesIosBackOutline
   /// assets/images/ios_back_outline.svg
-  static const String imagesIosBackOutline =
-      "assets/images/ios_back_outline.svg";
+  static const String imagesIosBackOutline = "assets/images/ios_back_outline.svg";
 
   /// Assets for imagesLikesHeart
   /// assets/images/likes_heart.svg
@@ -131,13 +123,12 @@ class Assets {
   static const String imagesPlusIcon = "assets/images/plus_icon.svg";
 
   /// Assets for imagesProfile
-  /// assets/images/Profile.png
-  static const String imagesProfile = "assets/images/Profile.png";
+  /// assets/images/profile.svg
+  static const String imagesProfile = "assets/images/profile.svg";
 
   /// Assets for imagesRectangleFoodIngredientPictureTest
   /// assets/images/rectangle_food_ingredient_picture_test.png
-  static const String imagesRectangleFoodIngredientPictureTest =
-      "assets/images/rectangle_food_ingredient_picture_test.png";
+  static const String imagesRectangleFoodIngredientPictureTest = "assets/images/rectangle_food_ingredient_picture_test.png";
 
   /// Assets for imagesSaladTest
   /// assets/images/salad_test.png
@@ -183,3 +174,4 @@ class Assets {
   /// assets/images/user_two_test.png
   static const String imagesUserTwoTest = "assets/images/user_two_test.png";
 }
+
