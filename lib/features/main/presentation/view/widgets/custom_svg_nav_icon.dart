4bc26def624c9fbaf8ce72a3cf3c7e191d477ab9
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-14 22:00:35 +0200

    create custom svg nav icon widget

---
+++ lib/features/main/presentation/view/widgets/custom_svg_nav_icon.dart
@@ -0,0 +1,21 @@
+import 'package:chefio_app/core/utils/theme_colors_extension.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CustomSvgNavIcon extends StatelessWidget {
+  final String asset;
+  final bool isSelected;
+  
+  const CustomSvgNavIcon({required this.asset, required this.isSelected, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      asset,
+      colorFilter: ColorFilter.mode(
+        isSelected ? context.primaryColor : context.secondaryTextColor,
+        BlendMode.srcIn,
+      ),
+    );
+  }
+}
