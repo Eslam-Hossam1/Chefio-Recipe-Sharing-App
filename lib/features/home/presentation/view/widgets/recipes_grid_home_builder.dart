8c73232b45703343c816f03e95ce9132ad949b0d
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-14 21:58:27 +0200

    create recipes grid home builder

---
+++ lib/features/home/presentation/view/widgets/recipes_grid_home_builder.dart
@@ -0,0 +1,70 @@
+import 'dart:developer';
+
+import 'package:chefio_app/core/Functions/show_custom_toast.dart';
+import 'package:chefio_app/core/utils/assets.dart';
+import 'package:chefio_app/core/widgets/sliver_adaptive_padding.dart';
+import 'package:chefio_app/features/home/data/models/recipe_model/recipe_model.dart';
+import 'package:chefio_app/features/home/presentation/manager/cubit/home_cubit.dart';
+import 'package:chefio_app/features/home/presentation/view/widgets/sliver_recipes_grid.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+class RecipesGridHomeBuilder extends StatelessWidget {
+  const RecipesGridHomeBuilder({super.key});
+  static var recipes = [
+    RecipeModel(title: "Eslam Hossam"),
+    RecipeModel(title: "Eslam Hossam"),
+    RecipeModel(title: "Eslam Hossam"),
+    RecipeModel(title: "Eslam Hossam"),
+    RecipeModel(title: "Eslam Hossam"),
+    RecipeModel(title: "Eslam Hossam"),
+    RecipeModel(title: "Eslam Hossam"),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<HomeCubit, HomeState>(
+      listener: (context, state) {
+        if (state is HomeScrollingFailureApi) {
+          showCustomToast(
+            context,
+            message: state.errorLocalizationKey.tr(),
+          );
+        }
+      },
+      builder: (context, state) {
+        if (state is HomeSuccess ||
+            state is HomeScrollingFailureApi ||
+            state is HomeLoadingNextRecipes) {
+          return SliverRecipesGrid(recipes: context.read<HomeCubit>().recipes);
+        } else if (state is HomefirstLoading) {
+          return Skeletonizer.sliver(
+            enabled: true,
+            child: SliverRecipesGrid(recipes: recipes),
+          );
+        } else if (state is HomeFirstApiFetchFailure) {
+          return SliverFillRemaining(
+            hasScrollBody: false,
+            child: Center(
+              child: Text(
+                textAlign: TextAlign.center,
+                state.errorLocalizationKey.tr(),
+                style: TextStyle(fontSize: 17, color: Colors.white),
+              ),
+            ),
+          );
+        } else {
+          return SliverToBoxAdapter(
+            child: Center(
+              child: Text(
+                "lol ya man",
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
