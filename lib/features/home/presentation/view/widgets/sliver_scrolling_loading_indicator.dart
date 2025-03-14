e0679c67f26015134167a1f68851de4213d6ed21
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-14 21:57:27 +0200

    create sliver scrolling loading indicator

---
+++ lib/features/home/presentation/view/widgets/sliver_scrolling_loading_indicator.dart
@@ -0,0 +1,19 @@
+
+import 'package:chefio_app/core/widgets/custom_cicular_progress_indicator.dart';
+import 'package:flutter/material.dart';
+
+class SliverScrollingLoadingIndIcator extends StatelessWidget {
+  const SliverScrollingLoadingIndIcator({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Center(child: CustomCircularProgressIndicator(),),
+        ),
+      );
+  }
+}
