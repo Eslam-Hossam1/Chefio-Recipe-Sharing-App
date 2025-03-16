711d01830050e1bf1dceb6eb26e666987c60e75f
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-16 04:17:16 +0200

    create upload first step page

---
+++ lib/features/upload/presentation/view/widgets/upload_first_step_page.dart
@@ -0,0 +1,26 @@
+import 'package:chefio_app/core/utils/app_localization_keys.dart';
+import 'package:chefio_app/core/utils/styles.dart';
+import 'package:chefio_app/core/utils/theme_colors_extension.dart';
+import 'package:chefio_app/core/widgets/adaptive_padding.dart';
+import 'package:chefio_app/features/upload/presentation/view/widgets/sliver_uplaod_header.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+class UploadFirstStepPage extends StatelessWidget {
+  const UploadFirstStepPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptivePadding(
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(child: SizedBox(height: 12,),),
+          SliverUploadHeader(
+            currentStep: '1',
+            steps: '2',
+          ),
+        ],
+      ),
+    );
+  }
+}
