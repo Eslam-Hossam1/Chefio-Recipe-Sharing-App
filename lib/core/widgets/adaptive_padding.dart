49624e8a292a233bb4ff0f57bb1e5159401cbd23
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-16 15:23:58 +0200

    upload first step page ui done

--- lib/core/widgets/adaptive_padding.dart
+++ lib/core/widgets/adaptive_padding.dart
@@ -3,12 +3,18 @@ import 'package:chefio_app/core/utils/size_config.dart';
 import 'package:flutter/material.dart';
 
 class AdaptivePadding extends StatelessWidget {
-  const AdaptivePadding({super.key, required this.child});
+  const AdaptivePadding({super.key, required this.child, this.top});
   final Widget child;
+  final double? top;
   @override
   Widget build(BuildContext context) {
+    final horizontalPadding = getPadding(context);
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: getPadding(context)),
+      padding: EdgeInsets.only(
+        left: horizontalPadding,
+        right: horizontalPadding,
+        top: top ?? 0,
+      ),
       child: child,
     );
   }
--- lib/features/upload/presentation/view/widgets/upload_first_step_page.dart
+++ lib/features/upload/presentation/view/widgets/upload_first_step_page.dart
@@ -1,23 +1,12 @@
-import 'package:chefio_app/core/utils/app_localization_keys.dart';
-import 'package:chefio_app/core/utils/assets.dart';
-import 'package:chefio_app/core/utils/constants.dart';
-import 'package:chefio_app/core/utils/styles.dart';
-import 'package:chefio_app/core/utils/theme_colors_extension.dart';
+
 import 'package:chefio_app/core/widgets/adaptive_padding.dart';
-import 'package:chefio_app/core/widgets/custom_text_button.dart';
-import 'package:chefio_app/core/widgets/tablet_form_container.dart';
-import 'package:chefio_app/features/auth/presentation/view/widgets/custom_text_form_field.dart';
-import 'package:chefio_app/features/auth/presentation/view/widgets/custome_email_text_form_field.dart';
 import 'package:chefio_app/features/upload/presentation/view/widgets/add_cover_photo.dart';
 import 'package:chefio_app/features/upload/presentation/view/widgets/cooking_duration_section.dart';
 import 'package:chefio_app/features/upload/presentation/view/widgets/food_name_and_description_section.dart';
 import 'package:chefio_app/features/upload/presentation/view/widgets/next_button.dart';
 import 'package:chefio_app/features/upload/presentation/view/widgets/sliver_uplaod_header.dart';
-import 'package:dotted_border/dotted_border.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 
 class UploadFirstStepPage extends StatelessWidget {
   const UploadFirstStepPage({super.key});
@@ -25,20 +14,16 @@ class UploadFirstStepPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AdaptivePadding(
+      top:12,
       child: CustomScrollView(
         slivers: [
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 12,
-            ),
-          ),
           SliverUploadHeader(
             currentStep: '1',
             steps: '2',
           ),
           SliverToBoxAdapter(
             child: SizedBox(
-              height: 32,
+              height: 32.h,
             ),
           ),
           SliverToBoxAdapter(
@@ -46,13 +31,13 @@ class UploadFirstStepPage extends StatelessWidget {
           ),
           SliverToBoxAdapter(
             child: SizedBox(
-              height: 24,
+              height: 24.h,
             ),
           ),
           FoodNameAndDescriptionSection(),
           SliverToBoxAdapter(
             child: SizedBox(
-              height: 24,
+              height: 24.h,
             ),
           ),
           CookingDurationSection(),
