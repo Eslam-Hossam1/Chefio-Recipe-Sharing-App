c06ae560ba0f522c1f74e8bb7e086bd860d0639b
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-17 16:41:00 +0200

    refactor add ingredient button & create add step button

--- lib/features/upload/presentation/view/widgets/add_step_button.dart
+++ lib/features/upload/presentation/view/widgets/add_step_button.dart
@@ -1,22 +1,16 @@
 import 'package:chefio_app/core/utils/app_localization_keys.dart';
-import 'package:chefio_app/core/utils/styles.dart';
-import 'package:chefio_app/core/utils/theme_colors_extension.dart';
-import 'package:chefio_app/core/widgets/custom_text_button.dart';
 import 'package:chefio_app/features/upload/presentation/view/widgets/add_button.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-class AddIngredintButton extends StatelessWidget {
-  const AddIngredintButton({
-    super.key,
-    this.onPressed, 
-  });
+class AddStepButton extends StatelessWidget {
+  const AddStepButton({super.key, this.onPressed});
   final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return AddButton(
       onPressed: onPressed,
-      text: AppLocalizationKeys.upload.addIngredient.tr(),
+      text: AppLocalizationKeys.upload.addStep.tr(),
     );
   }
 }
