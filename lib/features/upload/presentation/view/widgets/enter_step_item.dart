7c91a9c09759660c21c85c70cf560f8fda2fb6be
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-17 16:41:22 +0200

    modify enter step item

--- lib/features/upload/presentation/view/widgets/enter_step_item.dart
+++ lib/features/upload/presentation/view/widgets/enter_step_item.dart
@@ -1,8 +1,8 @@
-
 import 'package:chefio_app/core/utils/app_localization_keys.dart';
 import 'package:chefio_app/core/utils/styles.dart';
 import 'package:chefio_app/core/utils/theme_colors_extension.dart';
 import 'package:chefio_app/features/auth/presentation/view/widgets/custom_text_form_field.dart';
+import 'package:chefio_app/features/upload/presentation/view/widgets/add_step_photo_button.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -10,8 +10,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class EnterStepItem extends StatelessWidget {
   const EnterStepItem({
     super.key,
+    required this.stepIndex,
   });
-
+  final int stepIndex;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -23,7 +24,7 @@ class EnterStepItem extends StatelessWidget {
               radius: 12,
               backgroundColor: context.mainTextColor,
               child: Text(
-                '1',
+                '${stepIndex + 1}',
                 style: Styles.textStyleBold12(context).copyWith(
                   color: Colors.white,
                 ),
@@ -43,10 +44,18 @@ class EnterStepItem extends StatelessWidget {
           width: 8.w,
         ),
         Expanded(
-          child: CustomTextFormField(
-            hint: AppLocalizationKeys.upload.stepHint.tr(),
-            borderRadius: 8,
-            maxLines: 4,
+          child: Column(
+            children: [
+              CustomTextFormField(
+                hint: AppLocalizationKeys.upload.stepHint.tr(),
+                borderRadius: 8,
+                maxLines: 4,
+              ),
+              SizedBox(
+                height: 8.h,
+              ),
+              AddStepPhotoButton(),
+            ],
           ),
         ),
       ],
