a682c96b75da0686d7620286e781f3e071f9dbb5
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-20 21:08:31 +0200

    update enter ingredient item to work with upload recipe cubit and to take initial value or rebuild to handle delete other ingredient item that is before it

--- lib/features/upload/presentation/view/widgets/enter_ingredient_item.dart
+++ lib/features/upload/presentation/view/widgets/enter_ingredient_item.dart
@@ -1,15 +1,19 @@
-
+import 'package:chefio_app/core/Functions/form_validators.dart';
 import 'package:chefio_app/core/utils/app_localization_keys.dart';
 import 'package:chefio_app/core/utils/theme_colors_extension.dart';
 import 'package:chefio_app/features/auth/presentation/view/widgets/custom_text_form_field.dart';
+import 'package:chefio_app/features/upload/presentation/manager/upload_recipe_cubit/upload_recipe_cubit.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class EnterIngredientItem extends StatelessWidget {
   const EnterIngredientItem({
     super.key,
+    required this.ingredientIndex,
   });
+  final int ingredientIndex;
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +29,16 @@ class EnterIngredientItem extends StatelessWidget {
         ),
         Expanded(
           child: CustomTextFormField(
+            initialValue:
+                context.read<UploadRecipeCubit>().ingredients[ingredientIndex],
+            onSaved: (ingredient) {
+              context.read<UploadRecipeCubit>().ingredients[ingredientIndex] =
+                  ingredient!;
+            },
+            onChanged: (ingredientChange) {
+              context.read<UploadRecipeCubit>().ingredients[ingredientIndex] =
+                  ingredientChange;
+            },
             hint: AppLocalizationKeys.upload.enterIngredient.tr(),
           ),
         ),
