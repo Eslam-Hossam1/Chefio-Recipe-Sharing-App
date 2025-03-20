c706f96be5f6eafed25bd6e1e756b0b1d78f0fe8
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-20 20:40:34 +0200

    add custom bottom navigation bar item builder

--- lib/features/main/presentation/view/utils/bottom_nav_bar_item_builder.dart
+++ lib/features/main/presentation/view/utils/bottom_nav_bar_item_builder.dart
@@ -4,17 +4,17 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 BottomNavigationBarItem buildBottomNavigationBarItem(
-    BuildContext context, {
-    required String svgImagePath,
-    required String localizationKey,
-    required bool isSelected,
-  }) {
-    return BottomNavigationBarItem(
-      icon: CustomSvgNavIcon(
-        svgImagePath: svgImagePath,
-        isSelected: isSelected,
-      ),
-      label: localizationKey.tr(),
-      backgroundColor: context.scaffoldBackgroundColor,
-    );
-  }
+  BuildContext context, {
+  required String svgImagePath,
+  required String localizationKey,
+  required bool isSelected,
+}) {
+  return BottomNavigationBarItem(
+    icon: CustomSvgNavIcon(
+      svgImagePath: svgImagePath,
+      isSelected: isSelected,
+    ),
+    label: localizationKey.tr(),
+    backgroundColor: context.scaffoldBackgroundColor,
+  );
+}
