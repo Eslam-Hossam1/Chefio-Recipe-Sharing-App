2f9829a193c55b388baa07d0b0118057376f982a
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-20 21:03:48 +0200

    next button widget done part 1

--- lib/features/upload/presentation/view/widgets/next_button.dart
+++ lib/features/upload/presentation/view/widgets/next_button.dart
@@ -1,16 +1,24 @@
+import 'dart:developer';
+
 import 'package:chefio_app/core/utils/app_localization_keys.dart';
 import 'package:chefio_app/core/utils/styles.dart';
 import 'package:chefio_app/core/widgets/custom_text_button.dart';
+import 'package:chefio_app/features/upload/presentation/manager/upload_recipe_cubit/upload_recipe_cubit.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NextButton extends StatelessWidget {
   const NextButton({
     super.key,
     required this.onNext,
+    required this.formKey,
+    required this.enableAutoValidation,
   });
   final VoidCallback onNext;
+  final GlobalKey<FormState> formKey;
+  final VoidCallback enableAutoValidation;
   @override
   Widget build(BuildContext context) {
     return Align(
@@ -18,7 +26,21 @@ class NextButton extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.only(bottom: 24.h),
         child: CustomTextButton(
-          onPressed: onNext,
+          onPressed: () {
+            if (formKey.currentState!.validate()) {
+              formKey.currentState!.save();
+              log(context.read<UploadRecipeCubit>().foodName);
+              log(context.read<UploadRecipeCubit>().foodDescription);
+              log(context
+                  .read<UploadRecipeCubit>()
+                  .foodCookDuration
+                  .toString());
+              onNext();
+            } else {
+              enableAutoValidation();
+              log('not valid in first step');
+            }
+          },
           width: double.infinity,
           child: Text(
             AppLocalizationKeys.global.next.tr(),
