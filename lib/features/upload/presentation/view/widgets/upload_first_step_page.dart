39b8440ffd7627302190eddca805614697c6fed5
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-20 21:10:24 +0200

    update upload first step page : wrap with form and provide form key to next button

--- lib/features/upload/presentation/view/widgets/upload_first_step_page.dart
+++ lib/features/upload/presentation/view/widgets/upload_first_step_page.dart
@@ -1,10 +1,14 @@
+import 'dart:developer';
+
 import 'package:chefio_app/core/widgets/adaptive_padding.dart';
+import 'package:chefio_app/features/upload/presentation/manager/upload_recipe_cubit/upload_recipe_cubit.dart';
 import 'package:chefio_app/features/upload/presentation/view/widgets/add_cover_photo.dart';
 import 'package:chefio_app/features/upload/presentation/view/widgets/cooking_duration_section.dart';
 import 'package:chefio_app/features/upload/presentation/view/widgets/food_name_and_description_section.dart';
 import 'package:chefio_app/features/upload/presentation/view/widgets/next_button.dart';
 import 'package:chefio_app/features/upload/presentation/view/widgets/sliver_uplaod_header.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class UploadFirstStepPage extends StatefulWidget {
@@ -19,45 +23,58 @@ class _UploadFirstStepPageState extends State<UploadFirstStepPage>
     with AutomaticKeepAliveClientMixin {
   @override
   bool get wantKeepAlive => true;
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  void enableAutoValidation() {
+    setState(() {
+      autovalidateMode = AutovalidateMode.always;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
     return AdaptivePadding(
       top: 12,
-      child: CustomScrollView(
-        slivers: [
-          SliverUploadHeader(
-            currentStep: '1',
-            steps: '2',
-          ),
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 32.h,
+      child: Form(
+        key: formKey,
+        autovalidateMode: autovalidateMode,
+        child: CustomScrollView(
+          slivers: [
+            SliverUploadHeader(
+              currentStep: '1',
+              steps: '2',
             ),
-          ),
-          SliverToBoxAdapter(
-            child: AddCoverPhoto(),
-          ),
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 24.h,
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 32.h,
+              ),
             ),
-          ),
-          FoodNameAndDescriptionSection(),
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 24.h,
+            SliverToBoxAdapter(
+              child: AddCoverPhoto(),
             ),
-          ),
-          CookingDurationSection(),
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: NextButton(
-              onNext: widget.onNext,
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 24.h,
+              ),
             ),
-          ),
-        ],
+            FoodNameAndDescriptionSection(),
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 24.h,
+              ),
+            ),
+            CookingDurationSection(),
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: NextButton(
+                  enableAutoValidation: enableAutoValidation,
+                  onNext: widget.onNext,
+                  formKey: formKey),
+            ),
+          ],
+        ),
       ),
     );
   }
