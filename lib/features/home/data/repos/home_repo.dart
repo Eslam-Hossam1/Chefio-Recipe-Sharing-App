f682460878d6f062609b817ade3c639d0a888371
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-20 04:49:30 +0200

    update home repo & repo imple to work with the recipe new model and with the api correctly

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,9 +1,9 @@
 import 'package:chefio_app/core/errors/api_failure.dart';
 import 'package:chefio_app/core/errors/failures.dart';
-import 'package:chefio_app/features/home/data/models/recipe_model/recipe_model.dart';
+import 'package:chefio_app/features/home/data/models/home_success_model/recipe.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<ApiFailure, List<RecipeModel>>> fetchRecipesFromApi(
-      {required String categoryName, required int limit, required int skip});
+  Future<Either<ApiFailure, List<Recipe>>> fetchRecipesFromApi(
+      {required String categoryName, required int limit, required int page});
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,10 +1,13 @@
+import 'dart:developer';
+
 import 'package:chefio_app/core/api/api_consumer.dart';
 import 'package:chefio_app/core/api/api_keys.dart';
 import 'package:chefio_app/core/api/dio_consumer.dart';
 import 'package:chefio_app/core/api/end_ponits.dart';
 import 'package:chefio_app/core/errors/api_failure.dart';
 import 'package:chefio_app/core/errors/dio_api_failure.dart';
-import 'package:chefio_app/features/home/data/models/recipe_model/recipe_model.dart';
+import 'package:chefio_app/features/home/data/models/home_success_model/home_success_model.dart';
+import 'package:chefio_app/features/home/data/models/home_success_model/recipe.dart';
 import 'package:chefio_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
@@ -13,20 +16,18 @@ class HomeRepoImpl implements HomeRepo {
   final ApiConsumer _apiConsumer;
   HomeRepoImpl(this._apiConsumer);
   @override
-  Future<Either<DioApiFailure, List<RecipeModel>>> fetchRecipesFromApi(
-      {required String categoryName,required int limit, required int skip}) async {
+  Future<Either<DioApiFailure, List<Recipe>>> fetchRecipesFromApi(
+      {required String categoryName,
+      required int limit,
+      required int page}) async {
     try {
-      final response = await _apiConsumer.get(EndPoints.home, queryParameters: {
+      final response =
+          await _apiConsumer.get(EndPoints.recipes, queryParameters: {
         ApiKeys.limit: limit,
-        ApiKeys.skip: skip,
+        ApiKeys.page: page,
+        ApiKeys.category: categoryName,
       });
-      List<dynamic> products = response['products'];
-      final List<RecipeModel> recipes = products
-          .map(
-            (recipeJson) => RecipeModel.fromJson(recipeJson),
-          )
-          .toList();
-
+      final List<Recipe> recipes = HomeSuccessModel.fromJson(response).recipes!;
       return Right(recipes);
     } catch (e) {
       if (e is DioException) {
