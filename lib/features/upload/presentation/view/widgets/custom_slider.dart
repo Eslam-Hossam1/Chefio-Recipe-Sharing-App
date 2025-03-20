22ddd04ad7938712494875513f8da4ccf15430be
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-20 21:02:34 +0200

    make custom slider work with upload recipe cubit

--- lib/features/upload/presentation/view/widgets/custom_slider.dart
+++ lib/features/upload/presentation/view/widgets/custom_slider.dart
@@ -1,7 +1,8 @@
-
 import 'package:chefio_app/core/utils/theme_colors_extension.dart';
+import 'package:chefio_app/features/upload/presentation/manager/upload_recipe_cubit/upload_recipe_cubit.dart';
 import 'package:chefio_app/features/upload/presentation/view/widgets/slider_duration_label.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Customslider extends StatefulWidget {
   const Customslider({
@@ -48,6 +49,10 @@ class _CustomsliderState extends State<Customslider> {
           value: currentValue,
           min: 0,
           max: 60,
+          onChangeEnd: (cookDuration) {
+            context.read<UploadRecipeCubit>().foodCookDuration =
+                cookDuration.toInt();
+          },
           onChanged: (value) {
             setState(() {
               currentValue = value;
@@ -58,4 +63,3 @@ class _CustomsliderState extends State<Customslider> {
     );
   }
 }
-
