d76d745e0d3d359d21788002bec66a23cdf9155c
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-20 21:01:45 +0200

    back and next buttons widget done part 1

--- lib/features/upload/presentation/view/widgets/back_and_next_buttons.dart
+++ lib/features/upload/presentation/view/widgets/back_and_next_buttons.dart
@@ -1,14 +1,25 @@
+import 'dart:developer';
+
 import 'package:chefio_app/core/utils/app_localization_keys.dart';
 import 'package:chefio_app/core/utils/styles.dart';
 import 'package:chefio_app/core/utils/theme_colors_extension.dart';
 import 'package:chefio_app/core/widgets/custom_text_button.dart';
+import 'package:chefio_app/features/upload/presentation/manager/upload_recipe_cubit/upload_recipe_cubit.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BackAndNextButtons extends StatelessWidget {
-  const BackAndNextButtons({super.key, required this.onBack});
+  const BackAndNextButtons(
+      {super.key,
+      required this.onBack,
+      required this.formKey,
+      required this.enableAutoValidation});
   final VoidCallback onBack;
+  final GlobalKey<FormState> formKey;
+  final VoidCallback enableAutoValidation;
+
   @override
   Widget build(BuildContext context) {
     return Align(
@@ -33,7 +44,20 @@ class BackAndNextButtons extends StatelessWidget {
             ),
             Expanded(
               child: CustomTextButton(
-                width: double.infinity,
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    log(context
+                        .read<UploadRecipeCubit>()
+                        .ingredients
+                        .toString());
+                    log(context.read<UploadRecipeCubit>().steps.toString());
+                  } else {
+                    enableAutoValidation();
+
+                    log('not valid in second step');
+                  }
+                },
                 child: Text(
                   AppLocalizationKeys.global.next.tr(),
                   style: Styles.textStyleBold15(context)
