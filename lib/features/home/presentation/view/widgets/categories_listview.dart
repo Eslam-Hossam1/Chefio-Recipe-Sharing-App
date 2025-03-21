4d4a73d3131004e1ba30bdd50e02b737a56d3822
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-21 06:59:08 +0200

    update categories list view to be reusable

--- lib/features/home/presentation/view/widgets/categories_listview.dart
+++ lib/features/home/presentation/view/widgets/categories_listview.dart
@@ -3,38 +3,49 @@ import 'package:chefio_app/core/utils/constants.dart';
 import 'package:chefio_app/core/utils/size_config.dart';
 import 'package:chefio_app/features/home/data/enums/category_type_enum.dart';
 import 'package:chefio_app/features/home/data/models/category_model.dart';
+import 'package:chefio_app/features/home/data/models/home_success_model/category.dart';
 import 'package:chefio_app/features/home/presentation/manager/cubit/home_cubit.dart';
 import 'package:chefio_app/features/home/presentation/view/widgets/category_button.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CategoriesListView extends StatefulWidget {
-  const CategoriesListView({super.key});
-
+  const CategoriesListView(
+      {super.key, required this.categories, required this.onCategoryPressed});
+  final List<Category> categories;
+  final Function(String categoryName) onCategoryPressed;
   @override
   State<CategoriesListView> createState() => _CategoriesListViewState();
 }
 
 class _CategoriesListViewState extends State<CategoriesListView> {
-  List<CategoryModel> categories = [
-    CategoryModel.fromCategoryType(categoryType: CategoryType.all),
-    CategoryModel.fromCategoryType(categoryType: CategoryType.food),
-    CategoryModel.fromCategoryType(categoryType: CategoryType.drink),
-  ];
+  late int selectedCategoryIndex;
+  @override
+  void initState() {
+    super.initState();
+    selectedCategoryIndex = 0;
+  }
+  // List<CategoryModel> categories = [
+  //   CategoryModel.fromCategoryType(categoryType: CategoryType.generalDishes),
+  //   CategoryModel.fromCategoryType(categoryType: CategoryType.mainDishes),
+  //   CategoryModel.fromCategoryType(
+  //       categoryType: CategoryType.dessertsAndBakery),
+  // ];
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width;
     return BlocBuilder<HomeCubit, HomeState>(
       builder: (context, state) {
-        CategoryType selectedCategoryType =
-            context.read<HomeCubit>().categoryType;
+        // CategoryType selectedCategoryType =
+        //     context.read<HomeCubit>().categoryType;
         return SizedBox(
           height: 48,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
-            itemCount: categories.length,
+            itemCount: widget.categories.length,
             padding: EdgeInsetsDirectional.only(
                 start: width < SizeConfig.tabletBreakPoint
                     ? Constants.kMobileHorizontalPadding
@@ -44,12 +55,18 @@ class _CategoriesListViewState extends State<CategoriesListView> {
                 padding: EdgeInsetsDirectional.only(
                     end: width < SizeConfig.tabletBreakPoint ? 16 : 24),
                 child: CategoryButton(
-                  isSelected:
-                      selectedCategoryType == categories[index].categoryType,
-                  applocalizationKey: categories[index].applocalizationKey,
+                  isSelected: selectedCategoryIndex == index,
+                  categoryName:
+                      widget.categories[index].categoryLocalizationKey?.tr() ??
+                          widget.categories[index].name,
+                  // onPressed: () {
+                  //   context.read<HomeCubit>().fetchRecipesWithChangeCategory(
+                  //       categorytype: categories[index].categoryType);
+                  // },
                   onPressed: () {
-                    context.read<HomeCubit>().fetchRecipesWithChangeCategory(
-                        categorytype: categories[index].categoryType);
+                    setState(() {
+                      selectedCategoryIndex = index;
+                    });
                   },
                 ),
               );
