99c6f46875781670aaabc721fbe2405337be3e44
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-21 07:00:30 +0200

    add skeletonizer categories list view

---
+++ lib/features/home/presentation/view/widgets/skeletonizer_categories_list_view.dart
@@ -0,0 +1,16 @@
+import 'package:chefio_app/features/home/data/models/home_success_model/category.dart';
+import 'package:chefio_app/features/home/presentation/view/widgets/categories_listview.dart';
+import 'package:flutter/material.dart';
+
+class SkeletonizerCategoriesListView extends StatelessWidget {
+  const SkeletonizerCategoriesListView({super.key});
+    static List<Category> categories = List.generate(
+    5,
+    (index) => Category(name: "burger"),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return CategoriesListView(categories: categories, onCategoryPressed: (_){});
+  }
+}
