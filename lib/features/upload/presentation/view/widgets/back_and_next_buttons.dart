6add9d6ed2df75642e033a2f19604748a12566e6
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-25 00:55:19 +0200

    back and next buttons trigger cubit

--- lib/features/upload/presentation/view/widgets/back_and_next_buttons.dart
+++ lib/features/upload/presentation/view/widgets/back_and_next_buttons.dart
@@ -4,7 +4,7 @@ import 'package:chefio_app/core/utils/app_localization_keys.dart';
 import 'package:chefio_app/core/utils/styles.dart';
 import 'package:chefio_app/core/utils/theme_colors_extension.dart';
 import 'package:chefio_app/core/widgets/custom_text_button.dart';
-import 'package:chefio_app/features/upload/presentation/manager/upload_recipe_cubit/upload_recipe_cubit.dart';
+import 'package:chefio_app/features/upload/presentation/manager/set_recipe_cubit/upload_recipe_cubit.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -47,15 +47,9 @@ class BackAndNextButtons extends StatelessWidget {
                 onPressed: () {
                   if (formKey.currentState!.validate()) {
                     formKey.currentState!.save();
-                    log(context
-                        .read<UploadRecipeCubit>()
-                        .ingredients
-                        .toString());
-                    log(context.read<UploadRecipeCubit>().steps.toString());
+                    context.read<UploadRecipeCubit>().setRecipe();
                   } else {
                     enableAutoValidation();
-
-                    log('not valid in second step');
                   }
                 },
                 child: Text(
