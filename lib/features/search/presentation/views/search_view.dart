1b720ad12d77113a4777296859392a7657c1ad16
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-25 00:47:36 +0200

    add search view (not completed)

---
+++ lib/features/search/presentation/views/search_view.dart
@@ -0,0 +1,38 @@
+import 'package:chefio_app/core/utils/routing/routs.dart';
+import 'package:chefio_app/core/widgets/custom_text_button.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CustomTextButton(
+          child: Text(
+            'recipe details',
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () {
+            context.push(RoutePaths.recipeDetails);
+          },
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        CustomTextButton(
+          child: Text(
+            'recipe details deep',
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () {
+                        context.go(RoutePaths.recipeDetailsDeep,);
+          },
+        ),
+      ],
+    );
+  }
+}
