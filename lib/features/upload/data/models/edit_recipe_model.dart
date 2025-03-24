a2c636b56ce9ae8eefce95fa04a821619aa2c2d5
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-25 00:48:02 +0200

    add edit recipe model

---
+++ lib/features/upload/data/models/edit_recipe_model.dart
@@ -0,0 +1,35 @@
+import 'package:chefio_app/core/api/api_keys.dart';
+import 'package:dio/dio.dart';
+
+class EditRecipeModel {
+  final int id;
+  final MultipartFile? foodImage;
+  final String foodName;
+  final String foodDescription;
+  final String categoryName;
+  final int foodCookDuration;
+  final List<String> ingredients;
+  final List<String> steps;
+
+  const EditRecipeModel({
+    required this.id,
+    required this.ingredients,
+    required this.steps,
+    required this.foodName,
+    required this.foodDescription,
+    required this.foodCookDuration,
+    required this.categoryName,
+    this.foodImage,
+  });
+
+  Map<String, dynamic> toJson() => {
+        ApiKeys.id: id,
+        ApiKeys.recipePicture: foodImage,
+        ApiKeys.foodName: foodName,
+        ApiKeys.description: foodDescription,
+        ApiKeys.cookingDuration: foodCookDuration,
+        ApiKeys.category: categoryName,
+        ApiKeys.ingredients: ingredients,
+        ApiKeys.steps: steps
+      };
+}
