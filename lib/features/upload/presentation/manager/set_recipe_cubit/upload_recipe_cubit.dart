f775b81f360fc162ef4cab3547160f66eba5ca59
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-25 00:50:40 +0200

    add upload recipe cubit

---
+++ lib/features/upload/presentation/manager/set_recipe_cubit/upload_recipe_cubit.dart
@@ -0,0 +1,172 @@
+import 'dart:io';
+import 'package:bloc/bloc.dart';
+import 'package:chefio_app/core/Functions/convert_to_multipart.dart';
+import 'package:chefio_app/core/models/category.dart';
+import 'package:chefio_app/features/upload/data/models/edit_recipe_model.dart';
+import 'package:chefio_app/features/upload/data/models/recipe_detail_model.dart';
+import 'package:chefio_app/features/upload/data/models/upload_recipe_model.dart';
+import 'package:chefio_app/features/upload/data/repos/upload_repo.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+part 'upload_recipe_state.dart';
+
+class UploadRecipeCubit extends Cubit<UploadRecipeState> {
+  final UploadRepo _uploadRepo;
+  bool isEdit = false;
+  int? id;
+  List<String> ingredients = [""];
+  List<String> steps = [""];
+  String foodName = '';
+  String foodDescription = '';
+  int foodCookDuration = 1;
+  String categoryName = '';
+  File? foodImage;
+  List<Category> categories = [
+    Category(name: "meal"),
+    Category(name: "meal"),
+    Category(name: "meal"),
+    Category(name: "meal"),
+  ];
+
+  UploadRecipeCubit(
+    this._uploadRepo,
+  ) : super(UploadRecipeInitial());
+
+  void addIngerdient({
+    required GlobalKey<SliverAnimatedListState> ingredientsAnimatedListKey,
+  }) {
+    ingredients.add("");
+    ingredientsAnimatedListKey.currentState!.insertItem(ingredients.length - 1);
+  }
+
+  void addStep({
+    required GlobalKey<SliverAnimatedListState> stepsAnimatedListKey,
+  }) {
+    steps.add("");
+    stepsAnimatedListKey.currentState!.insertItem(steps.length - 1);
+  }
+
+  void removeIngredient({
+    required GlobalKey<SliverAnimatedListState> ingredientsAnimatedListKey,
+    required int index,
+  }) {
+    ingredientsAnimatedListKey.currentState!.removeItem(index,
+        (context, animation) {
+      return SizedBox(width: 0, height: 0);
+    });
+    ingredients.removeAt(index);
+  }
+
+  void removeStep({
+    required GlobalKey<SliverAnimatedListState> stepsAnimatedListKey,
+    required int index,
+  }) {
+    stepsAnimatedListKey.currentState!.removeItem(index, (context, animation) {
+      return SizedBox(width: 0, height: 0);
+    });
+    steps.removeAt(index);
+  }
+
+  void checkAndInitForEditing({required RecipeDetailModel? recipeDetailModel}) {
+    if (recipeDetailModel == null) return;
+    foodName = recipeDetailModel.foodName;
+    foodDescription = recipeDetailModel.foodDescription;
+    foodCookDuration = recipeDetailModel.foodCookDuration;
+    categoryName = recipeDetailModel.categoryName;
+    ingredients = recipeDetailModel.ingredients;
+    steps = recipeDetailModel.steps;
+    isEdit = true;
+    id = recipeDetailModel.id;
+  }
+
+  Future<void> fetchCategories() async {
+    emit(LoadingCategories());
+    var result = await _uploadRepo.fetchCategories();
+    result.fold((failure) {
+      emit(
+        CategoriesFailed(
+          errorMessage: failure.errMsg,
+          errorLocalizationKey: failure.localizaitonKey,
+        ),
+      );
+    }, (categories) {
+      this.categories = categories;
+      categoryName = categories[0].name;
+      emit(
+        CategoriesLoaded(),
+      );
+    });
+  }
+
+  bool validateImage() {
+    if (isEdit == false && foodImage == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  Future<void> setRecipe() async {
+    if (isEdit) {
+      await editRecipe();
+    } else {
+      await uploadRecipe();
+    }
+  }
+
+  Future<void> editRecipe() async {
+    EditRecipeModel editRecipeModel = _getEditRecipeModel();
+    //place edit request here
+    var result = await _uploadRepo.editRecipe(editRecipeModel: editRecipeModel);
+    result.fold((failure) {
+      emit(
+        UploadRecipeFailed(
+            errorMessage: failure.errMsg,
+            errorLocalizationKey: failure.localizaitonKey),
+      );
+    }, (_) {
+      emit(UploadRecipeSuccess());
+    });
+  }
+
+  Future<void> uploadRecipe() async {
+    UploadRecipeModel uploadRecipeModel = _getUploadRecipeModel();
+    var result =
+        await _uploadRepo.uploadRecipe(uploadRecipeModel: uploadRecipeModel);
+    result.fold((failure) {
+      emit(
+        UploadRecipeFailed(
+            errorMessage: failure.errMsg,
+            errorLocalizationKey: failure.localizaitonKey),
+      );
+    }, (_) {
+      emit(UploadRecipeSuccess());
+    });
+  }
+
+  UploadRecipeModel _getUploadRecipeModel() {
+    return UploadRecipeModel(
+      ingredients: ingredients,
+      steps: steps,
+      foodName: foodName,
+      foodDescription: foodDescription,
+      foodCookDuration: foodCookDuration,
+      categoryName: categoryName,
+      foodImage: convertToMultipart(foodImage)!,
+    );
+  }
+
+  EditRecipeModel _getEditRecipeModel() {
+    return EditRecipeModel(
+      id: id!,
+      ingredients: ingredients,
+      steps: steps,
+      foodName: foodName,
+      foodDescription: foodDescription,
+      foodCookDuration: foodCookDuration,
+      categoryName: categoryName,
+      foodImage: convertToMultipart(foodImage),
+    );
+  }
+}
