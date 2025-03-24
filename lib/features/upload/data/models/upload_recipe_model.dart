f4ffe9c395eba7dd957f4cf72a43833aead6803c
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-25 00:49:22 +0200

    add upload recipe model

---
+++ lib/features/upload/data/models/upload_recipe_model.dart
@@ -0,0 +1,32 @@
+import 'package:chefio_app/core/api/api_keys.dart';
+import 'package:dio/dio.dart';
+
+class UploadRecipeModel {
+  final MultipartFile foodImage;
+  final String foodName;
+  final String foodDescription;
+  final String categoryName;
+  final int foodCookDuration;
+  final List<String> ingredients;
+  final List<String> steps;
+
+  const UploadRecipeModel({
+    required this.ingredients,
+    required this.steps,
+    required this.foodName,
+    required this.foodDescription,
+    required this.foodCookDuration,
+    required this.categoryName,
+    required this.foodImage,
+  });
+
+  Map<String, dynamic> toJson() => {
+        ApiKeys.recipePicture: foodImage,
+        ApiKeys.foodName: foodName,
+        ApiKeys.description: foodDescription,
+        ApiKeys.cookingDuration: foodCookDuration,
+        ApiKeys.category: categoryName,
+        ApiKeys.ingredients: ingredients,
+        ApiKeys.steps: steps
+      };
+}
