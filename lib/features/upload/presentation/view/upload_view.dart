31907e63522394d069dce9cb0f7417b785ef9375
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-25 00:51:33 +0200

    integrate and trigger upload cubit in upload view

--- lib/features/upload/presentation/view/upload_view.dart
+++ lib/features/upload/presentation/view/upload_view.dart
@@ -1,15 +1,18 @@
-import 'package:chefio_app/core/utils/constants.dart';
-import 'package:chefio_app/features/upload/presentation/manager/upload_recipe_cubit/upload_recipe_cubit.dart';
+import 'package:chefio_app/core/utils/dialog_helper.dart';
+import 'package:chefio_app/core/widgets/custom_cicular_progress_indicator.dart';
+import 'package:chefio_app/features/home/presentation/view/widgets/custom_text_error_message.dart';
+import 'package:chefio_app/features/upload/data/models/recipe_detail_model.dart';
+import 'package:chefio_app/features/upload/presentation/manager/set_recipe_cubit/upload_recipe_cubit.dart';
+import 'package:chefio_app/features/upload/presentation/view/widgets/set_recipe_success_dialog.dart';
+import 'package:chefio_app/features/upload/presentation/view/widgets/set_recipe_second_step_page.dart';
 import 'package:chefio_app/features/upload/presentation/view/widgets/upload_first_step_page.dart';
-import 'package:chefio_app/features/upload/presentation/view/widgets/upload_second_step_page.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UploadView extends StatefulWidget {
-  const UploadView({super.key});
-
+  const UploadView({super.key, this.recipeDetailModel});
+  final RecipeDetailModel? recipeDetailModel;
   @override
   State<UploadView> createState() => _UploadViewState();
 }
@@ -35,20 +38,60 @@ class _UploadViewState extends State<UploadView> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    context.read<UploadRecipeCubit>().checkAndInitForEditing(
+          recipeDetailModel: widget.recipeDetailModel,
+        );
+    context.read<UploadRecipeCubit>().fetchCategories();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => UploadRecipeCubit(),
-      child: Scaffold(
-          body: SafeArea(
-        child: PageView(
-          controller: _pageController,
-          physics: NeverScrollableScrollPhysics(),
-          children: [
-            UploadFirstStepPage(onNext: _nextPage),
-            UploadSecondStepPage(onBack: _previousPage),
-          ],
-        ),
-      )),
-    );
+    return Scaffold(
+        body: SafeArea(
+      child: BlocConsumer<UploadRecipeCubit, UploadRecipeState>(
+        listener: (context, state) {
+          if (state is UploadRecipeFailed) {
+            DialogHelper.showErrorDialog(
+              context,
+              errorMessage: state.errorLocalizationKey.tr(),
+            );
+          } else if (state is UploadRecipeSuccess) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return SetRecipeSuccessDialog();
+              },
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is CategoriesFailed) {
+            return Padding(
+              padding: const EdgeInsets.all(32.0),
+              child:
+                  CustomTextErrorMessage(text: state.errorLocalizationKey.tr()),
+            );
+          } else if (state is LoadingCategories) {
+            return Center(
+              child: CustomCircularProgressIndicator(),
+            );
+          } else {
+            return PageView(
+              controller: _pageController,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                UploadFirstStepPage(
+                  onNext: _nextPage,
+                  recipeDetailModel: widget.recipeDetailModel,
+                ),
+                UploadSecondStepPage(onBack: _previousPage),
+              ],
+            );
+          }
+        },
+      ),
+    ));
   }
 }
