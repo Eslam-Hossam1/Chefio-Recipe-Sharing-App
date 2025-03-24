a827c1722e08f9686cb0af848923643328098b52
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-25 00:50:22 +0200

    add upload repo & repo impl

---
+++ lib/features/upload/data/repos/upload_repo_impl.dart
@@ -0,0 +1,65 @@
+import 'package:chefio_app/core/api/api_consumer.dart';
+import 'package:chefio_app/core/api/end_ponits.dart';
+import 'package:chefio_app/core/errors/api_failure.dart';
+import 'package:chefio_app/core/errors/dio_api_failure.dart';
+import 'package:chefio_app/core/models/category.dart';
+import 'package:chefio_app/core/utils/categories_service.dart';
+import 'package:chefio_app/features/upload/data/models/edit_recipe_model.dart';
+import 'package:chefio_app/features/upload/data/models/upload_recipe_model.dart';
+import 'package:chefio_app/features/upload/data/repos/upload_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class UploadRepoImpl implements UploadRepo {
+  final ApiConsumer _apiConsumer;
+  final CategoriesService _categoriesService;
+
+  UploadRepoImpl(this._apiConsumer, this._categoriesService);
+
+  @override
+  Future<Either<ApiFailure, void>> uploadRecipe(
+      {required UploadRecipeModel uploadRecipeModel}) async {
+    try {
+      await _apiConsumer.post(EndPoints.uploadRecipe,
+          data: uploadRecipeModel.toJson(), isFromData: true);
+      return Right(null);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(DioApiFailure.fromDioException(e));
+      } else {
+        return Left(DioApiFailure.unknown(e.toString()));
+      }
+    }
+  }
+
+  @override
+  Future<Either<ApiFailure, List<Category>>> fetchCategories() async {
+    try {
+      if (_categoriesService.categories.isEmpty) {
+        await _categoriesService.fetchAndSetCategories();
+      }
+      return Right(_categoriesService.categories);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(DioApiFailure.fromDioException(e));
+      } else {
+        return Left(DioApiFailure.unknown(e.toString()));
+      }
+    }
+  }
+
+  @override
+  Future<Either<ApiFailure, void>> editRecipe({required EditRecipeModel editRecipeModel})async {
+    try {
+      await _apiConsumer.patch(EndPoints.getEditRecipeEndPoint(editRecipeModel.id),
+          data: editRecipeModel.toJson(), isFromData: true);
+      return Right(null);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(DioApiFailure.fromDioException(e));
+      } else {
+        return Left(DioApiFailure.unknown(e.toString()));
+      }
+    }
+  }
+}
