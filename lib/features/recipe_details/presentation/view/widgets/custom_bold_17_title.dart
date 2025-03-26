5e7b67df5e68b4a109bab38b523f2229cccac004
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-26 08:57:27 +0200

    create custom bold 17 title widget for reusability

---
+++ lib/features/recipe_details/presentation/view/widgets/custom_bold_17_title.dart
@@ -0,0 +1,25 @@
+import 'package:chefio_app/core/utils/styles.dart';
+import 'package:chefio_app/core/utils/theme_colors_extension.dart';
+import 'package:flutter/material.dart';
+
+class CustomBold17Title extends StatelessWidget {
+  const CustomBold17Title({
+    super.key,
+    required this.text,
+  });
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          text,
+          style: Styles.textStyleBold17(context).copyWith(
+            color: context.mainTextColor,
+          ),
+        ),
+      ],
+    );
+  }
+}
