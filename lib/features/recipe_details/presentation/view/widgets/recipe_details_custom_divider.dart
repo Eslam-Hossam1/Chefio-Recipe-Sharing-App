85b20d9f2515cd44457f27a07f1569c63f62494a
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-27 05:54:06 +0200

    create recipe details custom divider

---
+++ lib/features/recipe_details/presentation/view/widgets/recipe_details_custom_divider.dart
@@ -0,0 +1,23 @@
+import 'package:chefio_app/core/utils/theme_colors_extension.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class RecipeDetailsCustomDivider extends StatelessWidget {
+  const RecipeDetailsCustomDivider({
+    super.key,
+    this.thickness = 1,
+    this.height = 32,
+    this.color,
+  });
+  final double thickness;
+  final double height;
+  final Color? color;
+  @override
+  Widget build(BuildContext context) {
+    return Divider(
+      thickness: thickness,
+      color: color??context.outlineColor,
+      height: height,
+    );
+  }
+}
