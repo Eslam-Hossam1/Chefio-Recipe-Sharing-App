9f4c4c030c60d055b9918800f4e6c6835133dade
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-28 01:46:21 +0200

    create recipe details cubit

---
+++ lib/features/recipe_details/presentation/manager/recipe_details_cubit/recipe_details_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:chefio_app/features/recipe_details/data/models/recipe_detail_model.dart';
+import 'package:chefio_app/features/recipe_details/data/repos/recipe_details_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'recipe_details_state.dart';
+
+class RecipeDetailsCubit extends Cubit<RecipeDetailsState> {
+  final RecipeDetailsRepo _recipeDetailsRepo;
+  RecipeDetailModel? recipeDetailModel;
+  RecipeDetailsCubit(this._recipeDetailsRepo) : super(RecipeDetailsInitial());
+  Future<void> fetchRecipeDetails({required String recipeId}) async {
+    emit(RecipeDetailsLoading());
+    final result =
+        await _recipeDetailsRepo.fetchRecipeDetails(recipeId: recipeId);
+    result.fold(
+        (failure) => emit(RecipeDetailsFailure(
+            errorMessage: failure.errMsg,
+            errorLocalizationKey: failure.localizaitonKey)),
+        (recipeDetailModel) {
+      this.recipeDetailModel = recipeDetailModel;
+      emit(
+        RecipeDetailsSuccess(),
+      );
+    });
+  }
+}
---
+++ lib/features/recipe_details/presentation/manager/recipe_details_cubit/recipe_details_state.dart
@@ -0,0 +1,22 @@
+part of 'recipe_details_cubit.dart';
+
+sealed class RecipeDetailsState extends Equatable {
+  const RecipeDetailsState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class RecipeDetailsInitial extends RecipeDetailsState {}
+final class RecipeDetailsFailure extends RecipeDetailsState {
+     final String errorMessage;
+  final String errorLocalizationKey;
+
+const  RecipeDetailsFailure({required this.errorMessage, required this.errorLocalizationKey});
+
+}
+final class RecipeDetailsLoading extends RecipeDetailsState {}
+
+final class RecipeDetailsSuccess extends RecipeDetailsState {
+  const RecipeDetailsSuccess();
+}
