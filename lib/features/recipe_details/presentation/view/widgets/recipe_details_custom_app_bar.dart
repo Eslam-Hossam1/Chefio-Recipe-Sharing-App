459f98ffacede0fb115925acf44207ea763b9726
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-27 07:51:49 +0200

    refactor recipe details app bar

--- lib/features/recipe_details/presentation/view/widgets/recipe_details_custom_app_bar.dart
+++ lib/features/recipe_details/presentation/view/widgets/recipe_details_custom_app_bar.dart
@@ -1,8 +1,11 @@
+import 'dart:developer';
 import 'dart:ui';
 
 import 'package:chefio_app/core/utils/assets.dart';
+import 'package:chefio_app/core/utils/styles.dart';
 import 'package:chefio_app/features/recipe_details/presentation/view/widgets/custom_circle_glass_button.dart';
 import 'package:chefio_app/features/recipe_details/presentation/view/widgets/recipe_details_app_bar_bottom.dart';
+import 'package:chefio_app/features/recipe_details/presentation/view/widgets/recipe_details_back_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
@@ -14,17 +17,22 @@ class RecipeDetailsCustomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double leadingItemHeight = 56;
+    double bottomheight = 48;
     double appBarImageInitialHeight = MediaQuery.sizeOf(context).height * .46;
-    double collapseHeight = (MediaQuery.sizeOf(context).height * .1 + 28)
-        .clamp((kToolbarHeight * 1.5), (kToolbarHeight * 2));
+    double collapseHeight =
+        (MediaQuery.sizeOf(context).height * .1 + (leadingItemHeight / 2))
+            .clamp((kToolbarHeight * 1.5), (kToolbarHeight * 2));
     return SliverAppBar(
       pinned: true,
       expandedHeight: appBarImageInitialHeight,
       flexibleSpace: LayoutBuilder(builder: (context, constraints) {
         // Calculate the collapse ratio
-        double collapseRatio = (constraints.maxHeight - collapseHeight) /
-            (MediaQuery.sizeOf(context).height * .4 - collapseHeight);
+        double collapseRatio =
+            (constraints.maxHeight - collapseHeight - bottomheight - 8) /
+                (appBarImageInitialHeight - collapseHeight - bottomheight - 8);
         collapseRatio = collapseRatio.clamp(0.0, 1.0);
+        // log('maxHeight: ${constraints.maxHeight} , collapseheight: ${collapseHeight}  , expandedheight: ${appBarImageInitialHeight} , collapseRatio: ${collapseRatio}');
 
         return Stack(
           fit: StackFit.expand,
@@ -38,36 +46,28 @@ class RecipeDetailsCustomAppBar extends StatelessWidget {
             // Dark Overlay: Becomes more visible as the AppBar collapses
             AnimatedOpacity(
               duration: Duration(milliseconds: 300),
-              opacity: 1 - collapseRatio,
+              opacity: (1 - collapseRatio).clamp(0.0, 0.5),
               child: Container(
                 // duration: Duration(milliseconds: 300),
                 color: Colors.grey.shade900,
               ),
             ),
 
-            // Recipe Title: Appears when the AppBar is collapsed
             Positioned(
-              top: collapseHeight / 2, // Align title at toolbar height
+              top: collapseHeight / 2,
               left: 16.w,
-
               right: 16.w,
               child: Row(
                 children: [
-                  CustomCircleGlassButton(
-                    child: SvgPicture.asset(
-                      Assets.imagesIosBackOutline,
-                      colorFilter: ColorFilter.mode(
-                        Colors.white,
-                        BlendMode.srcIn,
-                      ),
-                    ),
-                  ),
+                  RecipeDetailsBackButton(),
                   SizedBox(
                     width: 12.w,
                   ),
+                  // Recipe Title: Appears when the AppBar is collapsed
+
                   Expanded(
                     child: AnimatedOpacity(
-                      opacity: collapseRatio <= 0.5
+                      opacity: collapseRatio <= 0.3
                           ? 1.0
                           : 0.0, // Show when collapsed
                       duration: Duration(milliseconds: 300),
@@ -75,10 +75,8 @@ class RecipeDetailsCustomAppBar extends StatelessWidget {
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
                         "Recipe Titleasjkdfkasjdfkljaskldfjaskldfja;klsdfjkasdjklasdjfklasdfj;",
-                        style: TextStyle(
+                        style: Styles.textStyleBold17(context).copyWith(
                           color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
                         ),
                         textAlign: TextAlign.start,
                       ),
