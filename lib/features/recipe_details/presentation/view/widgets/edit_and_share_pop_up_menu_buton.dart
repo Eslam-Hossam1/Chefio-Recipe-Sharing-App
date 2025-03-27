bf96e5c3f1b644c587b29e454f887e60dfb1cf4d
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-27 09:30:09 +0200

    create edit and share pop up menu button widget

---
+++ lib/features/recipe_details/presentation/view/widgets/edit_and_share_pop_up_menu_buton.dart
@@ -0,0 +1,59 @@
+
+import 'package:chefio_app/core/utils/styles.dart';
+import 'package:chefio_app/core/utils/theme_colors_extension.dart';
+import 'package:chefio_app/features/recipe_details/presentation/view/widgets/custom_circle_glass_button.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class EditAndSharePopUpMenuButton extends StatelessWidget {
+  const EditAndSharePopUpMenuButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      color: context.dialogBackgroundColor,
+      child: CustomCircleGlassButton(
+        child: Icon(
+          FontAwesomeIcons.ellipsisVertical,
+          color: Colors.white,
+        ),
+      ),
+      itemBuilder: (context) {
+        return [
+          PopupMenuItem(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "Share Recipe",
+                  style: Styles.textStyleMedium15(context).copyWith(
+                    color: context.mainTextColor,
+                  ),
+                ),
+                Icon(Icons.share, color: context.mainTextColor)
+              ],
+            ),
+            onTap: () {},
+          ),
+          PopupMenuItem(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "Edit Recipe",
+                    style: Styles.textStyleMedium15(context).copyWith(
+                      color: context.mainTextColor,
+                    ),
+                  ),
+                  Icon(Icons.edit, color: context.mainTextColor)
+                ],
+              ),
+              onTap: () {})
+        ];
+      },
+    );
+  }
+}
+
