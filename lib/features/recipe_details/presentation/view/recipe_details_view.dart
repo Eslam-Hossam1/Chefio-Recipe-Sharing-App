a84e048a392df74609b64a928111510f2b1fe1f2
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-28 02:27:49 +0200

    integrate recipe detais view with recipe details cubit

--- lib/features/recipe_details/presentation/view/recipe_details_view.dart
+++ lib/features/recipe_details/presentation/view/recipe_details_view.dart
@@ -1,27 +1,54 @@
-import 'package:chefio_app/core/utils/constants.dart';
 import 'package:chefio_app/core/utils/routing/routs.dart';
+import 'package:chefio_app/core/widgets/custom_cicular_progress_indicator.dart';
+import 'package:chefio_app/features/recipe_details/presentation/manager/recipe_details_cubit/recipe_details_cubit.dart';
+import 'package:chefio_app/features/recipe_details/presentation/view/widgets/recipe_details_failure_body.dart';
 import 'package:chefio_app/features/recipe_details/presentation/view/widgets/recipe_details_view_body.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
-class RecipeDetailsView extends StatelessWidget {
-  const RecipeDetailsView({super.key});
+class RecipeDetailsView extends StatefulWidget {
+  const RecipeDetailsView({super.key, required this.id});
+  final String id;
+  @override
+  State<RecipeDetailsView> createState() => _RecipeDetailsViewState();
+}
+
+class _RecipeDetailsViewState extends State<RecipeDetailsView> {
+  @override
+  void initState() {
+    context.read<RecipeDetailsCubit>().fetchRecipeDetails(recipeId: widget.id);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return PopScope(
       canPop: false,
       onPopInvokedWithResult: (didPop, result) async {
         if (didPop) return;
-         if (context.canPop()) {
+        if (context.canPop()) {
           context.pop();
         } else {
-            context.go('${RoutePaths.home}/${RoutePaths.search}');
-          }
-        
+          context.go('${RoutePaths.home}/${RoutePaths.search}');
+        }
       },
       child: Scaffold(
-        body: RecipeDetailsViewBody(),
+        body: BlocBuilder<RecipeDetailsCubit, RecipeDetailsState>(
+          builder: (context, state) {
+            if (state is RecipeDetailsFailure) {
+              return RecipeDetailsFailureBody(
+                  text: state.errorLocalizationKey.tr(),);
+            } else if (state is RecipeDetailsSuccess) {
+              return RecipeDetailsViewBody();
+            } else {
+              return Center(
+                child: CustomCircularProgressIndicator(),
+              );
+            }
+          },
+        ),
       ),
     );
   }
---
+++ lib/features/recipe_details/presentation/view/widgets/recipe_details_failure_body.dart
@@ -0,0 +1,26 @@
+
+import 'package:chefio_app/core/widgets/custom_text_button.dart';
+import 'package:chefio_app/features/home/presentation/view/widgets/custom_text_error_message.dart';
+import 'package:flutter/material.dart';
+
+class RecipeDetailsFailureBody extends StatelessWidget {
+  const RecipeDetailsFailureBody({
+    super.key, required this.text,
+  });
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CustomTextErrorMessage(
+          text:text,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        CustomTextButton(child: Text('try again'))
+      ],
+    );
+  }
+}
