4d0b6f0b1abf472fd6188a6d936220f8c4f08395
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-29 01:50:54 +0200

    make categories depend on interface not the implementation & append categories for testing , will be removed soon

--- lib/core/utils/categories_service.dart
+++ lib/core/utils/categories_service.dart
@@ -1,13 +1,14 @@
+import 'package:chefio_app/core/api/api_consumer.dart';
 import 'package:chefio_app/core/api/dio_consumer.dart';
 import 'package:chefio_app/core/models/category.dart';
 
 class CategoriesService {
-  List<Category> categories = [];
-  final DioConsumer _dioConsumer;
+  List<Category> categories = [Category(name: 'meals'),Category(name: 'مقبلات'),];
+  final ApiConsumer _apiConsumer;
   final String _endPoint;
-  CategoriesService(this._dioConsumer, this._endPoint);
+  CategoriesService(this._apiConsumer, this._endPoint);
   Future<void> fetchAndSetCategories() async {
-    final response = await _dioConsumer.get(_endPoint);
+    final response = await _apiConsumer.get(_endPoint);
     categories =
         (response as List).map((json) => Category.fromJson(json)).toList();
   }
