a5cd873cbbe1eb1259f2013db1c87f9f6c4a0b5c
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-29 03:24:50 +0200

    create  search view body

--- lib/features/search/presentation/views/search_view.dart
+++ lib/features/search/presentation/views/search_view.dart
@@ -1,5 +1,6 @@
-import 'package:chefio_app/core/utils/routing/routs.dart';
+import 'package:chefio_app/core/utils/routing/routing_helper.dart';
 import 'package:chefio_app/core/widgets/custom_text_button.dart';
+import 'package:chefio_app/features/search/presentation/views/search_view_body.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -8,33 +9,6 @@ class SearchView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        CustomTextButton(
-          child: Text(
-            'recipe details',
-            style: TextStyle(color: Colors.white),
-          ),
-          onPressed: () {
-            context.push(RoutePaths.recipeDetails);
-          },
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        CustomTextButton(
-          child: Text(
-            'recipe details deep',
-            style: TextStyle(color: Colors.white),
-          ),
-          onPressed: () {
-            context.go(
-              RoutePaths.recipeDetails,
-            );
-          },
-        ),
-      ],
-    );
+    return SearchViewBody();
   }
 }
