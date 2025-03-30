405fa6ab969cbb10489e476596e7a4ef0386e996
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-30 22:00:28 +0200

    register search recipe repo and provide search recipe cubit

--- lib/core/utils/routing/shell_branches.dart
+++ lib/core/utils/routing/shell_branches.dart
@@ -2,6 +2,8 @@ import 'package:chefio_app/core/utils/routing/routs.dart';
 import 'package:chefio_app/core/utils/service_locator.dart';
 import 'package:chefio_app/features/home/data/repos/home_repo_impl.dart';
 import 'package:chefio_app/features/home/presentation/manager/cubit/home_cubit.dart';
+import 'package:chefio_app/features/search/data/repos/search_recipe_rebo_impl.dart';
+import 'package:chefio_app/features/search/presentation/manager/search_recipe_cubit/search_recipe_cubit.dart';
 import 'package:chefio_app/features/search/presentation/views/search_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -40,7 +42,11 @@ abstract class ShellBranches {
           routes: [
             GoRoute(
               path: RoutePaths.search,
-              builder: (context, state) => const SearchView(),
+              builder: (context, state) => BlocProvider(
+                create: (context) =>
+                    SearchRecipeCubit(getIt<SearchRecipeRepoImpl>()),
+                child: const SearchView(),
+              ),
             ),
           ],
         ),
