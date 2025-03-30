7bd2e68d8ee4153e2fa881d8da4fbb7dffd2a6dd
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-30 22:03:01 +0200

    make search text field take initial value from search recipe cubit for clean handling states

--- lib/features/search/presentation/views/widget/search_text_field.dart
+++ lib/features/search/presentation/views/widget/search_text_field.dart
@@ -1,10 +1,13 @@
+import 'dart:developer';
+
 import 'package:chefio_app/core/utils/app_localization_keys.dart';
 import 'package:chefio_app/core/utils/assets.dart';
-import 'package:chefio_app/core/utils/styles.dart';
 import 'package:chefio_app/core/utils/theme_colors_extension.dart';
 import 'package:chefio_app/core/widgets/custom_text_field.dart';
+import 'package:chefio_app/features/search/presentation/manager/search_recipe_cubit/search_recipe_cubit.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
 class SearchTextField extends StatefulWidget {
@@ -20,6 +23,8 @@ class _SearchTextFieldState extends State<SearchTextField> {
   final TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    textEditingController.text =
+        context.read<SearchRecipeCubit>().searchRecipeName;
     return CustomTextField(
       maxLines: 1,
       controller: textEditingController,
@@ -44,6 +49,16 @@ class _SearchTextFieldState extends State<SearchTextField> {
           ),
         ),
       ),
+      onSubmitted: (searchRecipeName) =>
+          context.read<SearchRecipeCubit>().fetchRecipesWithSearchName(
+                searchRecipeName: searchRecipeName,
+              ),
     );
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
   }
 }
