1236ba98a665a1ba9ca54a6793473a743929d9c9
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-03-30 06:27:44 +0200

    make filter icon open filter bottom sheet & give controller for clear on tap on cancle icon

--- lib/features/search/presentation/views/widget/search_text_field.dart
+++ lib/features/search/presentation/views/widget/search_text_field.dart
@@ -7,16 +7,23 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class SearchTextField extends StatelessWidget {
+class SearchTextField extends StatefulWidget {
   const SearchTextField({
     super.key,
   });
 
   @override
+  State<SearchTextField> createState() => _SearchTextFieldState();
+}
+
+class _SearchTextFieldState extends State<SearchTextField> {
+  final TextEditingController textEditingController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return CustomTextField(
+      maxLines: 1,
+      controller: textEditingController,
       textInputAction: TextInputAction.search,
-        
       hint: AppLocalizationKeys.global.search.tr(),
       prefixIcon: SvgPicture.asset(
         Assets.imagesSearch,
@@ -25,11 +32,16 @@ class SearchTextField extends StatelessWidget {
           BlendMode.srcIn,
         ),
       ),
-      suffixIcon: SvgPicture.asset(
-        Assets.imagesCloseIcon,
-        colorFilter: ColorFilter.mode(
-          context.mainTextColor,
-          BlendMode.srcIn,
+      suffixIcon: IconButton(
+        onPressed: () {
+          textEditingController.clear();
+        },
+        icon: SvgPicture.asset(
+          Assets.imagesCloseIcon,
+          colorFilter: ColorFilter.mode(
+            context.mainTextColor,
+            BlendMode.srcIn,
+          ),
         ),
       ),
     );
