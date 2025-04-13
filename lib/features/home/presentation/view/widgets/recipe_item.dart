3bbf0326afdbdfa58bb7c35dc18289343aed913c
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-04-13 12:21:10 +0200

    create recipe item body widget and make it clackable and when click it navigates to recipe details view

--- lib/features/home/presentation/view/widgets/recipe_item.dart
+++ lib/features/home/presentation/view/widgets/recipe_item.dart
@@ -5,6 +5,7 @@ import 'package:chefio_app/core/utils/styles.dart';
 import 'package:chefio_app/core/utils/theme_colors_extension.dart';
 import 'package:chefio_app/core/widgets/recipe_category_with_cooking_duration_text.dart';
 import 'package:chefio_app/features/home/data/models/home_success_model/recipe.dart';
+import 'package:chefio_app/features/home/presentation/view/widgets/recipe_item_body.dart';
 import 'package:chefio_app/features/home/presentation/view/widgets/recipe_item_header.dart';
 import 'package:chefio_app/features/home/presentation/view/widgets/recipe_item_image_with_icon.dart';
 import 'package:easy_localization/easy_localization.dart';
@@ -26,27 +27,8 @@ class RecipeItem extends StatelessWidget {
         SizedBox(
           height: 16,
         ),
-        Flexible(
-            child: RecipeItemImageWithIcon(
-          recipeModel: recipeModel,
-        )),
-        SizedBox(
-          height: 16,
-        ),
-        Text(
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          recipeModel.foodName ?? "null",
-          style: Styles.textStyleBold17(context)
-              .copyWith(color: context.mainTextColor),
-        ),
-        SizedBox(
-          height: 8,
-        ),
-        RecipeCategoryWithCookingDurationText(
-          categoryName: recipeModel.category.categoryLocalizationKey?.tr() ??
-              recipeModel.category.name,
-          cookingDuration: recipeModel.cookingDuration.toString(),
+        Expanded(
+          child: RecipeItemBody(recipeModel: recipeModel)
         ),
       ],
     );
---
+++ lib/features/home/presentation/view/widgets/recipe_item_body.dart
@@ -0,0 +1,53 @@
+import 'package:chefio_app/core/utils/routing/routing_helper.dart';
+import 'package:chefio_app/core/utils/styles.dart';
+import 'package:chefio_app/core/utils/theme_colors_extension.dart';
+import 'package:chefio_app/core/widgets/recipe_category_with_cooking_duration_text.dart';
+import 'package:chefio_app/features/home/data/models/home_success_model/recipe.dart';
+import 'package:chefio_app/features/home/presentation/view/widgets/recipe_item_image_with_icon.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class RecipeItemBody extends StatelessWidget {
+  const RecipeItemBody({super.key, required this.recipeModel});
+  final Recipe recipeModel;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.push(
+          RoutingHelper.getRecipeDetailsPath(
+            recipeId: recipeModel.id!,
+          ),
+        );
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Flexible(
+              child: RecipeItemImageWithIcon(
+            recipeModel: recipeModel,
+          )),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            recipeModel.foodName ?? "null",
+            style: Styles.textStyleBold17(context)
+                .copyWith(color: context.mainTextColor),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          RecipeCategoryWithCookingDurationText(
+            categoryName: recipeModel.category.categoryLocalizationKey?.tr() ??
+                recipeModel.category.name,
+            cookingDuration: recipeModel.cookingDuration.toString(),
+          ),
+        ],
+      ),
+    );
+  }
+}
