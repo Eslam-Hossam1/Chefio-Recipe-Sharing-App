4184c2b5341eef22119c916d97d68eca750561fe
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-04-13 12:33:43 +0200

    modify search app bar : make it with row instead of sliver app bar fields

--- lib/features/search/presentation/views/widget/search_app_bar.dart
+++ lib/features/search/presentation/views/widget/search_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:chefio_app/core/utils/assets.dart';
 import 'package:chefio_app/core/utils/constants.dart';
 import 'package:chefio_app/core/utils/theme_colors_extension.dart';
+import 'package:chefio_app/core/widgets/adaptive_padding.dart';
 import 'package:chefio_app/features/search/presentation/views/widget/filter_icon.dart';
 import 'package:chefio_app/features/search/presentation/views/widget/search_filter_bottom_sheet.dart';
 import 'package:chefio_app/features/search/presentation/views/widget/search_text_field.dart';
@@ -19,40 +20,79 @@ class SearchAppBar extends StatelessWidget {
       backgroundColor: context.scaffoldBackgroundColor,
       elevation: 0,
       automaticallyImplyLeading: false,
-      expandedHeight: 60,
+      expandedHeight: 80,
       pinned: false,
       floating: true,
+
       surfaceTintColor: Colors.transparent,
-      leading: IconButton(
-        onPressed: () {
-          context.pop();
-        },
-        icon: SvgPicture.asset(
-          Assets.imagesIosBackOutline,
-          colorFilter: ColorFilter.mode(
-            context.mainTextColor,
-            BlendMode.srcIn,
+      flexibleSpace: Center(
+        child: AdaptivePadding(
+          child: Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  context.pop();
+                },
+                icon: SvgPicture.asset(
+                  Assets.imagesIosBackOutline,
+                  colorFilter: ColorFilter.mode(
+                    context.mainTextColor,
+                    BlendMode.srcIn,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Expanded(child: SearchTextField()),
+              SizedBox(
+                width: 8,
+              ),
+              IconButton(
+                onPressed: () {
+                  showModalBottomSheet(
+                      backgroundColor: context.scaffoldBackgroundColor,
+                      context: context,
+                      builder: (context) {
+                        return SearchFilterBottomSheet();
+                      });
+                },
+                icon: FilterIcon(),
+              ),
+            ],
           ),
         ),
       ),
-      title: SearchTextField(),
-      centerTitle: true,
-      actions: [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: IconButton(
-            onPressed: () {
-              showModalBottomSheet(
-                  backgroundColor: context.scaffoldBackgroundColor,
-                  context: context,
-                  builder: (context) {
-                    return SearchFilterBottomSheet();
-                  });
-            },
-            icon: FilterIcon(),
-          ),
-        ),
-      ],
+      // leading: IconButton(
+      //   onPressed: () {
+      //     context.pop();
+      //   },
+      //   icon: SvgPicture.asset(
+      //     Assets.imagesIosBackOutline,
+      //     colorFilter: ColorFilter.mode(
+      //       context.mainTextColor,
+      //       BlendMode.srcIn,
+      //     ),
+      //   ),
+      // ),
+      // title: SearchTextField(),
+      // centerTitle: true,
+      // actions: [
+      //   Padding(
+      //     padding: EdgeInsets.symmetric(horizontal: 16),
+      //     child: IconButton(
+      //       onPressed: () {
+      //         showModalBottomSheet(
+      //             backgroundColor: context.scaffoldBackgroundColor,
+      //             context: context,
+      //             builder: (context) {
+      //               return SearchFilterBottomSheet();
+      //             });
+      //       },
+      //       icon: FilterIcon(),
+      //     ),
+      //   ),
+      // ],
     );
   }
 }
