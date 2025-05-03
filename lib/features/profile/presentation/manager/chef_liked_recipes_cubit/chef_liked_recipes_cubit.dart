11fe7f656a1b27f0f394f8bbfb8f2700489ab938
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-03 04:16:55 +0300

    create chef liked recipes cubit ( incompleted )

---
+++ lib/features/profile/presentation/manager/chef_liked_recipes_cubit/chef_liked_recipes_cubit.dart
@@ -0,0 +1,61 @@
+import 'package:bloc/bloc.dart';
+import 'package:chefio_app/features/profile/data/models/profile_chef_liked_recipe_model.dart';
+import 'package:chefio_app/features/profile/data/repos/profile_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'chef_liked_recipes_state.dart';
+
+class ChefLikedRecipesCubit extends Cubit<ChefLikedRecipesState> {
+  final ProfileRepo _profileRepo;
+  ChefLikedRecipesCubit({
+    required ProfileRepo profileRepo,
+  })  : _profileRepo = profileRepo,
+        super(ChefLikedRecipesInitial());
+  int page = 1;
+  int limit = 30;
+  bool hasMore = true;
+  bool isLoading = false;
+  List<ProfileChefLikedRecipeModel> chefLikedRecipes = [];
+  
+  Future<void> fetchChefLikedRecipes({required String chefId}) async {
+    if (isLoading || !hasMore) return;
+    isLoading = true;
+    if (chefLikedRecipes.isNotEmpty) {
+      emit(FetchMoreLikedRecipes());
+    } else {
+      emit(LikedRecipesInitialFetch());
+    }
+
+    final result = await _profileRepo.fetchChefLikedRecipes(
+      chefId: chefId,
+      limit: limit,
+      page: page,
+    );
+
+    result.fold((failure) {
+      if (chefLikedRecipes.isEmpty) {
+        emit(
+          LikedRecipesInitialFetchFailure(
+            errMsg: failure.errMsg,
+            errLocalizationKey: failure.localizaitonKey,
+          ),
+        );
+      } else {
+        emit(
+          FetchMoreLikedRecipesFailure(
+            errMsg: failure.errMsg,
+            errLocalizationKey: failure.localizaitonKey,
+          ),
+        );
+      }
+    }, (chefLikedRecipes) {
+      if (chefLikedRecipes.length <= limit) {
+        hasMore = false;
+      }
+      chefLikedRecipes.addAll(chefLikedRecipes);
+      page += 1;
+      isLoading = false;
+      emit(ChefLikedRecipesSuccess());
+    });
+  }
+}
---
+++ lib/features/profile/presentation/manager/chef_liked_recipes_cubit/chef_liked_recipes_state.dart
@@ -0,0 +1,38 @@
+part of 'chef_liked_recipes_cubit.dart';
+
+sealed class ChefLikedRecipesState extends Equatable {
+  const ChefLikedRecipesState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class ChefLikedRecipesInitial extends ChefLikedRecipesState {}
+
+final class LikedRecipesInitialFetch extends ChefLikedRecipesState {}
+
+final class LikedRecipesInitialFetchFailure extends ChefLikedRecipesState {
+  final String errMsg;
+  final String errLocalizationKey;
+
+  const LikedRecipesInitialFetchFailure({
+    required this.errMsg,
+    required this.errLocalizationKey,
+  });
+}
+
+final class FetchMoreLikedRecipes extends ChefLikedRecipesState {}
+
+final class ChefLikedRecipesFetchMore extends ChefLikedRecipesState {}
+
+final class FetchMoreLikedRecipesFailure extends ChefLikedRecipesState {
+  final String errMsg;
+  final String errLocalizationKey;
+
+  const FetchMoreLikedRecipesFailure({
+    required this.errMsg,
+    required this.errLocalizationKey,
+  });
+}
+
+final class ChefLikedRecipesSuccess extends ChefLikedRecipesState {}
