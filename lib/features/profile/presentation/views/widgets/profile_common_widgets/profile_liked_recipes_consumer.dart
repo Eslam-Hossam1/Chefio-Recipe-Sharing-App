ac947e79f0fdbb426e8ca111aabb56e6557c527f
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-09 00:34:37 +0300

    handle profile liked recipes consumer states

--- lib/features/profile/presentation/views/widgets/profile_common_widgets/profile_liked_recipes_consumer.dart
+++ lib/features/profile/presentation/views/widgets/profile_common_widgets/profile_liked_recipes_consumer.dart
@@ -1,7 +1,8 @@
 import 'package:chefio_app/core/Functions/show_custom_toast.dart';
-import 'package:chefio_app/features/home/presentation/view/widgets/custom_text_error_message.dart';
+import 'package:chefio_app/core/widgets/custom_info_message_with_button.dart';
+import 'package:chefio_app/features/home/presentation/view/widgets/custom_text_info_message.dart';
 import 'package:chefio_app/features/profile/presentation/manager/chef_liked_recipes_cubit/chef_liked_recipes_cubit.dart';
-import 'package:chefio_app/features/profile/presentation/views/widgets/profile_common_widgets/profile_liked_recipes_grid.dart';
+import 'package:chefio_app/features/profile/presentation/manager/profile_cubit/profile_cubit.dart';
 import 'package:chefio_app/features/profile/presentation/views/widgets/profile_common_widgets/profile_liked_recipes_section.dart';
 import 'package:chefio_app/features/profile/presentation/views/widgets/profile_common_widgets/skeletonizer_profile_liked_recipes_grid.dart';
 import 'package:easy_localization/easy_localization.dart';
@@ -26,11 +27,19 @@ class ProfileLikedRecipesConsumer extends StatelessWidget {
       },
       builder: (context, state) {
         if (state is LikedRecipesInitialFetchFailure) {
-          return CustomTextErrorMessage(
-            text: state.errLocalizationKey.tr(),
+          return CustomInfoMessageWithButton(
+            message: state.errLocalizationKey.tr(),
+            btnText: "Try Again",
+            onPressed: () {
+              context.read<ChefLikedRecipesCubit>().fetchChefLikedRecipes(
+                    chefId: context.read<ProfileCubit>().chefId,
+                  );
+            },
           );
         } else if (state is LikedRecipesInitialFetch) {
           return SkeletonizerProfileLikedRecipesGrid();
+        } else if (state is EmptyChefLikedRecipes) {
+          return CustomTextInfoMessage(text: "You didn't add any recipe yet");
         } else {
           return ProfileLikedRecipesSection();
         }
