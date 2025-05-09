89d5f230c6a7c71c579ee3c6b432ff18e0cfdf2d
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-09 18:10:39 +0300

    add recipes grid view as helper for skeletonizer profile recipes grid view

---
+++ lib/features/profile/presentation/views/widgets/profile_common_widgets/recipes_grid_view.dart
@@ -0,0 +1,74 @@
+import 'dart:developer';
+
+import 'package:chefio_app/core/Entities/recipe_entity.dart';
+import 'package:chefio_app/core/utils/size_config.dart';
+import 'package:chefio_app/features/home/presentation/view/widgets/recipe_item.dart';
+import 'package:flutter/material.dart';
+
+class RecipesGrid extends StatelessWidget {
+  const RecipesGrid({
+    super.key,
+    required this.recipes,
+  });
+
+  final List<RecipeEntity> recipes;
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.sizeOf(context).width;
+    log('Screen width: $width');
+
+    final bool isBigMobile =
+        width >= 420 && width < SizeConfig.tabletBreakPoint;
+    final bool isMobile = width >= 330 && width < 420;
+    final bool isSmallMobile = width < 330;
+    final bool isSmallTablet =
+        width >= SizeConfig.tabletBreakPoint && width < 650;
+    final bool isTablet = width >= 650 && width < 1000;
+    final bool isBigTablet = width >= 1000 && width < 1300;
+    final bool isDesktop = width >= 1300;
+
+    // نتحكم في maxCrossAxisExtent حسب حجم الشاشة
+    double maxCrossAxisExtent;
+    if (isSmallMobile) {
+      maxCrossAxisExtent = 220;
+    } else if (isMobile) {
+      maxCrossAxisExtent = 200;
+    } else if (isBigMobile) {
+      maxCrossAxisExtent = 240;
+    } else if (isSmallTablet) {
+      maxCrossAxisExtent = 215;
+    } else if (isTablet) {
+      maxCrossAxisExtent = 215;
+    } else if (isBigTablet) {
+      maxCrossAxisExtent = 250;
+    } else {
+      maxCrossAxisExtent = 300;
+    }
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: maxCrossAxisExtent,
+        childAspectRatio: isMobile
+            ? 0.502
+            : isBigMobile
+                ? .54
+                : isSmallTablet
+                    ? .527
+                    : isTablet
+                        ? .53
+                        : isBigTablet
+                            ? .56
+                            : isDesktop
+                                ? .60
+                                : 0.54,
+        crossAxisSpacing: width > SizeConfig.tabletBreakPoint ? 24 : 16,
+        mainAxisSpacing: width < 550 ? 8 : 16,
+      ),
+      itemBuilder: (context, index) {
+        return RecipeItem(recipeEntity: recipes[index]);
+      },
+      itemCount: recipes.length,
+    );
+  }
+}
---
+++ lib/features/profile/presentation/views/widgets/profile_common_widgets/skeletonizer_profile_recipes_grid_view.dart
@@ -0,0 +1,76 @@
+import 'dart:developer';
+
+import 'package:chefio_app/core/Entities/recipe_body_entity.dart';
+import 'package:chefio_app/core/utils/size_config.dart';
+import 'package:chefio_app/features/home/data/models/home_success_model/recipe_model.dart';
+import 'package:chefio_app/features/home/presentation/view/widgets/recipe_item_body.dart';
+import 'package:flutter/material.dart';
+
+class SkeletonizerProfileRecipesGridView extends StatelessWidget {
+  const SkeletonizerProfileRecipesGridView({
+    super.key,
+    required this.recipes,
+  });
+
+  final List<RecipeBodyEntity> recipes;
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.sizeOf(context).width;
+    log('Screen width: $width');
+
+    final bool isBigMobile =
+        width >= 420 && width < SizeConfig.tabletBreakPoint;
+    final bool isMobile = width >= 330 && width < 420;
+    final bool isSmallMobile = width < 330;
+    final bool isSmallTablet =
+        width >= SizeConfig.tabletBreakPoint && width < 650;
+    final bool isTablet = width >= 650 && width < 1000;
+    final bool isBigTablet = width >= 1000 && width < 1300;
+    final bool isDesktop = width >= 1300;
+
+    // نتحكم في maxCrossAxisExtent حسب حجم الشاشة
+    double maxCrossAxisExtent;
+    if (isSmallMobile) {
+      maxCrossAxisExtent = 180.84;
+    } else if (isMobile) {
+      maxCrossAxisExtent = 180.4;
+    } else if (isBigMobile) {
+      maxCrossAxisExtent = 240;
+    } else if (isSmallTablet) {
+      maxCrossAxisExtent = 176.73;
+    } else if (isTablet) {
+      maxCrossAxisExtent = 170;
+    } else if (isBigTablet) {
+      maxCrossAxisExtent = 205.5;
+    } else {
+      maxCrossAxisExtent = 246.6;
+    }
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: maxCrossAxisExtent,
+        childAspectRatio: isMobile
+            //هنا
+            ? 0.591
+            : isBigMobile
+                ? .64
+                : isSmallTablet
+                    ? .620806
+                    : isTablet
+                        ? 0.60
+                        : isBigTablet
+                            ? .6188
+                            : isDesktop
+                                ? .6788
+                                : 0.60,
+        crossAxisSpacing: width > SizeConfig.tabletBreakPoint ? 24 : 16,
+        mainAxisSpacing: width < 550 ? 8 : 16,
+      ),
+      itemBuilder: (context, index) {
+        return RecipeItemBody(recipeBodyEntity: recipes[index]);
+      },
+      itemCount: recipes.length,
+    );
+  }
+}
