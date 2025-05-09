1731d02c3ebf11dc51eccc242b24655239a02b1b
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-09 18:08:39 +0300

    share button in profile view done

--- lib/features/profile/presentation/views/widgets/profile_sliver_app_bar.dart
+++ lib/features/profile/presentation/views/widgets/profile_sliver_app_bar.dart
@@ -1,8 +1,12 @@
+import 'package:chefio_app/core/helpers/share_helper.dart';
 import 'package:chefio_app/core/utils/assets.dart';
+import 'package:chefio_app/core/utils/service_locator.dart';
 import 'package:chefio_app/core/utils/theme/theme_colors_extension.dart';
 import 'package:chefio_app/core/widgets/adaptive_padding.dart';
 import 'package:chefio_app/core/widgets/custom_back_icon_button.dart';
+import 'package:chefio_app/features/profile/presentation/manager/profile_cubit/profile_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 
@@ -23,7 +27,9 @@ class ProfileSliverAppBar extends StatelessWidget {
           children: [
             CustomBackIconButton(),
             IconButton(
-              onPressed: () {},
+              onPressed: () async => getIt<ShareHelper>().shareProfile(
+                chefId: context.read<ProfileCubit>().chefId,
+              ),
               icon: SvgPicture.asset(
                 Assets.imagesShare,
                 colorFilter: ColorFilter.mode(
