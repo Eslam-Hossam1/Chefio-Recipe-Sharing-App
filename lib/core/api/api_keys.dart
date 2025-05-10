dd94158313c530af340c4a2459c1e5c3f450e9d9
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-11 01:23:10 +0300

    add data api key

--- lib/core/api/api_keys.dart
+++ lib/core/api/api_keys.dart
@@ -37,6 +37,7 @@ abstract class ApiKeys {
   static const String profilePage = 'pageRecipes';
   static const String profileLimit = 'limitRecipes';
   static const String recipes = 'recipes';
+  static const String data = 'data';
   static const String following = 'following';
   static const String notFollowing = 'not_following';
   static const String followers = 'followers';
