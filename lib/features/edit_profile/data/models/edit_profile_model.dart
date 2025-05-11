0222f8f11fd16036d1d26335e0e1fa9312b79898
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-11 05:54:23 +0300

    create edit profile model

---
+++ lib/features/edit_profile/data/models/edit_profile_model.dart
@@ -0,0 +1,14 @@
+import 'package:chefio_app/core/api/api_keys.dart';
+import 'package:dio/dio.dart';
+
+class EditProfileModel {
+  final MultipartFile chefProfilePicture;
+  final String chefUsername;
+
+  const EditProfileModel(
+      {required this.chefProfilePicture, required this.chefUsername});
+  Map<String, dynamic> toJson() => {
+        ApiKeys.username: chefUsername,
+        ApiKeys.profilePicture: chefProfilePicture,
+      };
+}
