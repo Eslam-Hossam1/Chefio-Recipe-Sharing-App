c373f87e64f8f5dc9a9b1764aa2789262351d833
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-11 03:31:50 +0300

    create edit profile view

---
+++ lib/features/edit_profile/presentation/views/edit_profile_view.dart
@@ -0,0 +1,13 @@
+import 'package:chefio_app/features/edit_profile/presentation/views/widgets/edit_profile_body.dart';
+import 'package:flutter/material.dart';
+
+class EditProfileView extends StatelessWidget {
+  const EditProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      body: EditProfileBody(),
+    ));
+  }
+}
