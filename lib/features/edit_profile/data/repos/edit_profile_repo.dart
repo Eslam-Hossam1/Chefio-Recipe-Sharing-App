369a969ddd5bc292f6268bb8c8482e0c61888b71
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-11 05:55:06 +0300

    create edit profile repo

---
+++ lib/features/edit_profile/data/repos/edit_profile_repo.dart
@@ -0,0 +1,8 @@
+import 'package:chefio_app/core/errors/api_failure.dart';
+import 'package:chefio_app/features/edit_profile/data/models/edit_profile_model.dart';
+import 'package:chefio_app/features/edit_profile/data/models/success_edit_model/success_edit_profile_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class EditProfileRepo {
+  Future<Either<ApiFailure, SuccessEditProfileModel>> editProfile({required EditProfileModel editProfileModel});
+}
