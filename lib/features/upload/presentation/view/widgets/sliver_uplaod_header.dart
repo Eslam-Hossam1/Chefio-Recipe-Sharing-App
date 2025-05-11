eec2f7b44c50c86c3ba288fb229720be814dd0c6
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-11 03:54:02 +0300

    use cancel widget in sliver upload header instead of implementation

--- lib/features/upload/presentation/view/widgets/sliver_uplaod_header.dart
+++ lib/features/upload/presentation/view/widgets/sliver_uplaod_header.dart
@@ -1,9 +1,7 @@
-import 'package:chefio_app/core/utils/Localization/app_localization_keys/app_localization_keys.dart';
 import 'package:chefio_app/core/utils/styles.dart';
 import 'package:chefio_app/core/utils/theme/theme_colors_extension.dart';
-import 'package:easy_localization/easy_localization.dart';
+import 'package:chefio_app/core/widgets/custom_cancel.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class SliverUploadHeader extends StatelessWidget {
   const SliverUploadHeader({
@@ -19,17 +17,7 @@ class SliverUploadHeader extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          GestureDetector(
-            onTap: () {
-              context.pop();
-            },
-            child: Text(
-              AppLocalizationKeys.global.cancel.tr(),
-              style: Styles.textStyleBold17(context).copyWith(
-                color: context.secondaryColor,
-              ),
-            ),
-          ),
+          CustomCancel(),
           Row(
             children: [
               Text(
