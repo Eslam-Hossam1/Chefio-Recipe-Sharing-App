4e5b530d4e5a0e3ea26548583a5c26dde3b60805
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-11 03:53:39 +0300

    craete custom cancel widget

---
+++ lib/core/widgets/custom_cancel.dart
@@ -0,0 +1,28 @@
+
+import 'package:chefio_app/core/utils/Localization/app_localization_keys/app_localization_keys.dart';
+import 'package:chefio_app/core/utils/styles.dart';
+import 'package:chefio_app/core/utils/theme/theme_colors_extension.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class CustomCancel extends StatelessWidget {
+  const CustomCancel({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.pop();
+      },
+      child: Text(
+        AppLocalizationKeys.global.cancel.tr(),
+        style: Styles.textStyleBold17(context).copyWith(
+          color: context.secondaryColor,
+        ),
+      ),
+    );
+  }
+}
