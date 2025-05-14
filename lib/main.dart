a3329743fc0f0c9106061f54f21218cce6c78fcc
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-14 03:37:56 +0300

    firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:chefio_app/chefio_app.dart';
 import 'package:chefio_app/core/helpers/auth_credentials_helper.dart';
 import 'package:chefio_app/core/utils/app_bloc_observer.dart';
 import 'package:chefio_app/core/utils/service_locator.dart';
+import 'package:chefio_app/firebase_options.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -12,6 +14,9 @@ import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   Bloc.observer = AppBlocObserver();
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
