6bc52537eac102c3429f07202bf459998470f1ed
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-17 06:34:19 +0300

    rigester nofitications service

--- lib/core/services/notifications_service.dart
+++ lib/core/services/notifications_service.dart
@@ -1,3 +1,24 @@
-class NotificationsService{
-  
+import 'dart:developer';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class NotificationsService {
+  final FirebaseMessaging _firebaseMessaging;
+
+  NotificationsService({required FirebaseMessaging firebaseMessaging})
+      : _firebaseMessaging = firebaseMessaging;
+
+  Future<void> init() async {
+    await _firebaseMessaging.requestPermission();
+    String? token = await _firebaseMessaging.getToken();
+    log('FCM Token: ${token ?? 'null token'}');
+
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+  }
+
+  @pragma('vm:entry-point')
+  static Future<void> handleBackgroundMessage(RemoteMessage remoteMessage) async {
+    await Firebase.initializeApp();
+    log('🔔 Background message title: ${remoteMessage.notification?.title}');
+  }
 }
