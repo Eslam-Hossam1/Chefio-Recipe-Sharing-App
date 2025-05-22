d3948f9b2b71a01470494829714052fc4d361099
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-22 16:45:54 +0300

    add get notification image method

--- lib/core/services/local_notifications_service.dart
+++ lib/core/services/local_notifications_service.dart
@@ -1,41 +1,68 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class LocalNotificationsService {
   final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin;
+  final Dio _dio;
 
   LocalNotificationsService({
     required FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
-  }) : _flutterLocalNotificationsPlugin = flutterLocalNotificationsPlugin;
+    required Dio dio,
+  })  : _flutterLocalNotificationsPlugin = flutterLocalNotificationsPlugin,
+        _dio = dio;
 
   Future<void> init() async {
     InitializationSettings initializationSettings = InitializationSettings(
       android: AndroidInitializationSettings("@mipmap/ic_launcher"),
       iOS: DarwinInitializationSettings(),
     );
-    await _flutterLocalNotificationsPlugin.initialize(
-      initializationSettings,
-      onDidReceiveNotificationResponse: (details) {},
-      onDidReceiveBackgroundNotificationResponse: handleBackgroundNotificationTap
-    );
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse: (details) {},
+        onDidReceiveBackgroundNotificationResponse:
+            handleBackgroundNotificationTap);
   }
 
   @pragma('vm:entry-point')
-  static handleBackgroundNotificationTap(NotificationResponse notificationResponse){}
+  static handleBackgroundNotificationTap(
+      NotificationResponse notificationResponse) {}
 
-  Future<void> showBasicNotification() async {
+  Future<void> showBasicNotification(
+      {required RemoteMessage remoteMessage}) async {
+    BigPictureStyleInformation? bigPictureStyleInformation;
+    bigPictureStyleInformation = await getNotificationImage(remoteMessage, bigPictureStyleInformation);
+
     NotificationDetails notificationDetails = NotificationDetails(
       android: AndroidNotificationDetails(
         'channelId',
         'channelName',
         importance: Importance.max,
         priority: Priority.max,
+        styleInformation: bigPictureStyleInformation,
       ),
     );
     await _flutterLocalNotificationsPlugin.show(
       0,
-      'eslam title',
-      'eslam body',
+      remoteMessage.notification?.title,
+      remoteMessage.notification?.body,
       notificationDetails,
     );
+  }
+
+  Future<BigPictureStyleInformation?> getNotificationImage(RemoteMessage remoteMessage, BigPictureStyleInformation? bigPictureStyleInformation) async {
+     if (remoteMessage.notification?.android?.imageUrl != null) {
+      _dio.options.responseType = ResponseType.bytes;
+      final response =
+          await _dio.get(remoteMessage.notification?.android?.imageUrl ?? '');
+       bigPictureStyleInformation =
+          BigPictureStyleInformation(
+        ByteArrayAndroidBitmap.fromBase64String(base64Encode(response.data)),
+        largeIcon: ByteArrayAndroidBitmap.fromBase64String(
+            base64Encode(response.data)),
+      );
+    }
+    return bigPictureStyleInformation;
   }
 }
