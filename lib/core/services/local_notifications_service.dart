cc825b18a091bff009e79bce1097a22bf799427a
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-05-23 04:44:27 +0300

    specify explicit android channel to strict popping up a notification in background state

--- lib/core/services/local_notifications_service.dart
+++ lib/core/services/local_notifications_service.dart
@@ -19,10 +19,24 @@ class LocalNotificationsService {
       android: AndroidInitializationSettings("@mipmap/ic_launcher"),
       iOS: DarwinInitializationSettings(),
     );
-    await _flutterLocalNotificationsPlugin.initialize(initializationSettings,
-        onDidReceiveNotificationResponse: (details) {},
-        onDidReceiveBackgroundNotificationResponse:
-            handleBackgroundNotificationTap);
+    await _flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (details) {},
+      onDidReceiveBackgroundNotificationResponse:
+          handleBackgroundNotificationTap,
+    );
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      description:
+          'This channel is used for important notifications.', // description
+      importance: Importance.max,
+    );
+    
+    await _flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
   }
 
   @pragma('vm:entry-point')
@@ -32,7 +46,8 @@ class LocalNotificationsService {
   Future<void> showBasicNotification(
       {required RemoteMessage remoteMessage}) async {
     BigPictureStyleInformation? bigPictureStyleInformation;
-    bigPictureStyleInformation = await getNotificationImage(remoteMessage, bigPictureStyleInformation);
+    bigPictureStyleInformation =
+        await getNotificationImage(remoteMessage, bigPictureStyleInformation);
 
     NotificationDetails notificationDetails = NotificationDetails(
       android: AndroidNotificationDetails(
@@ -51,13 +66,14 @@ class LocalNotificationsService {
     );
   }
 
-  Future<BigPictureStyleInformation?> getNotificationImage(RemoteMessage remoteMessage, BigPictureStyleInformation? bigPictureStyleInformation) async {
-     if (remoteMessage.notification?.android?.imageUrl != null) {
+  Future<BigPictureStyleInformation?> getNotificationImage(
+      RemoteMessage remoteMessage,
+      BigPictureStyleInformation? bigPictureStyleInformation) async {
+    if (remoteMessage.notification?.android?.imageUrl != null) {
       _dio.options.responseType = ResponseType.bytes;
       final response =
           await _dio.get(remoteMessage.notification?.android?.imageUrl ?? '');
-       bigPictureStyleInformation =
-          BigPictureStyleInformation(
+      bigPictureStyleInformation = BigPictureStyleInformation(
         ByteArrayAndroidBitmap.fromBase64String(base64Encode(response.data)),
         largeIcon: ByteArrayAndroidBitmap.fromBase64String(
             base64Encode(response.data)),
