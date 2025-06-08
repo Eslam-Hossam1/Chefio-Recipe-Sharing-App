a880c2b74c02bc6f8be115c58abd25e72b80ae76
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-06-08 05:41:38 +0300

    add request in notifications view

--- lib/features/notifiactions/presentation/view/notifications_view.dart
+++ lib/features/notifiactions/presentation/view/notifications_view.dart
@@ -1,8 +1,21 @@
+import 'package:chefio_app/features/notifiactions/presentation/manager/notifications_cubit/notifications_cubit.dart';
 import 'package:chefio_app/features/notifiactions/presentation/view/widgets/notifications_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class NotificationsView extends StatelessWidget {
+class NotificationsView extends StatefulWidget {
   const NotificationsView({super.key});
+
+  @override
+  State<NotificationsView> createState() => _NotificationsViewState();
+}
+
+class _NotificationsViewState extends State<NotificationsView> {
+  @override
+  initState() {
+    super.initState();
+    context.read<NotificationsCubit>().fetchNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
