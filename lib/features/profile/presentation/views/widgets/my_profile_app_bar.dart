a60cde3368b80cc4725994eb7fe3c78b4907d87e
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-06-08 18:17:14 +0300

    add settings icon button in my profile app bar

--- lib/features/profile/presentation/views/widgets/my_profile_app_bar.dart
+++ lib/features/profile/presentation/views/widgets/my_profile_app_bar.dart
@@ -1,8 +1,10 @@
+import 'package:chefio_app/core/utils/routing/routs.dart';
 import 'package:chefio_app/core/utils/theme/theme_colors_extension.dart';
 import 'package:chefio_app/core/widgets/adaptive_padding.dart';
 import 'package:chefio_app/features/profile/presentation/views/widgets/my_profile_pop_up_menu_button.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class MyProfileAppBar extends StatelessWidget {
   const MyProfileAppBar({
@@ -17,7 +19,7 @@ class MyProfileAppBar extends StatelessWidget {
         children: [
           IconButton(
             onPressed: () {
-              Navigator.of(context).pop();
+              context.push(RoutePaths.settings);
             },
             icon: Icon(
               FontAwesomeIcons.gear,
