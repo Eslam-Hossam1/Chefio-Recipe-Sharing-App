cf3c9d74e11caea1e1c7bc03f2bac1e7413f4f78
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-06-09 03:50:30 +0300

    refactor settings view : move setting tile widget in seperate file

---
+++ lib/features/settings/presentation/views/settings_view.dart
@@ -0,0 +1,69 @@
+import 'package:chefio_app/core/cubit/theme_cubit/theme_cubit.dart';
+import 'package:chefio_app/core/utils/theme/theme_colors_extension.dart';
+import 'package:chefio_app/features/settings/presentation/views/language_selection_view.dart';
+import 'package:chefio_app/features/settings/presentation/views/theme_selection_view.dart';
+import 'package:chefio_app/features/settings/presentation/views/widgets/setting_tile.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+class SettingsView extends StatelessWidget {
+  const SettingsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.watch<ThemeCubit>().state;
+    final currentLocale = context.locale;
+
+    String getThemeName(ThemeMode mode) {
+      switch (mode) {
+        case ThemeMode.light:
+          return 'light'.tr();
+        case ThemeMode.dark:
+          return 'dark'.tr();
+        case ThemeMode.system:
+        default:
+          return 'system'.tr();
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('settings'.tr(), style: TextStyle(color: context.mainTextColor)),
+        backgroundColor: context.scaffoldBackgroundColor,
+        elevation: 0,
+        iconTheme: IconThemeData(color: context.mainTextColor),
+        surfaceTintColor: Colors.transparent,
+      ),
+      backgroundColor: context.scaffoldBackgroundColor,
+      body: ListView(
+        children: [
+          const SizedBox(height: 12),
+
+          // Theme setting
+          SettingTile(
+            title: 'theme'.tr(),
+            value: getThemeName(theme),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => const ThemeSelectionView()),
+            ),
+          ),
+
+          const Divider(),
+
+          // Language setting
+          SettingTile(
+            title: 'language'.tr(),
+            value: currentLocale.languageCode == 'ar' ? 'العربية' : 'English',
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => const LanguageSelectionView()),
+            ),
+          ),
+
+          const Divider(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/settings/presentation/views/widgets/setting_tile.dart
@@ -0,0 +1,32 @@
+
+import 'package:chefio_app/core/utils/theme/theme_colors_extension.dart';
+import 'package:flutter/material.dart';
+
+class SettingTile extends StatelessWidget {
+  final String title;
+  final String value;
+  final VoidCallback onTap;
+
+  const SettingTile({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title, style: TextStyle(color: context.mainTextColor, fontSize: 16)),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(value, style: TextStyle(color: context.secondaryTextColor)),
+          const Icon(Icons.arrow_forward_ios_rounded, size: 16),
+        ],
+      ),
+      onTap: onTap,
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+    );
+  }
+}
