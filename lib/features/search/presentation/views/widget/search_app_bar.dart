c6ed5efdb2710ed41313f7ee467a32e41f75fc64
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-06-19 09:51:54 +0300

    remove filter icon button from search app bar

--- lib/features/search/presentation/views/widget/search_app_bar.dart
+++ lib/features/search/presentation/views/widget/search_app_bar.dart
@@ -1,14 +1,8 @@
-import 'package:chefio_app/core/utils/assets.dart';
-import 'package:chefio_app/core/utils/constants.dart';
 import 'package:chefio_app/core/utils/theme/theme_colors_extension.dart';
 import 'package:chefio_app/core/widgets/adaptive_padding.dart';
 import 'package:chefio_app/core/widgets/custom_back_icon_button.dart';
-import 'package:chefio_app/features/search/presentation/views/widget/filter_icon.dart';
-import 'package:chefio_app/features/search/presentation/views/widget/search_filter_bottom_sheet.dart';
 import 'package:chefio_app/features/search/presentation/views/widget/search_text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:go_router/go_router.dart';
 
 class SearchAppBar extends StatelessWidget {
   const SearchAppBar({
@@ -24,7 +18,6 @@ class SearchAppBar extends StatelessWidget {
       expandedHeight: 80,
       pinned: false,
       floating: true,
-
       surfaceTintColor: Colors.transparent,
       flexibleSpace: Center(
         child: AdaptivePadding(
@@ -38,51 +31,10 @@ class SearchAppBar extends StatelessWidget {
               SizedBox(
                 width: 8,
               ),
-              IconButton(
-                onPressed: () {
-                  showModalBottomSheet(
-                      backgroundColor: context.scaffoldBackgroundColor,
-                      context: context,
-                      builder: (context) {
-                        return SearchFilterBottomSheet();
-                      });
-                },
-                icon: FilterIcon(),
-              ),
             ],
           ),
         ),
       ),
-      // leading: IconButton(
-      //   onPressed: () {
-      //     context.pop();
-      //   },
-      //   icon: SvgPicture.asset(
-      //     Assets.imagesIosBackOutline,
-      //     colorFilter: ColorFilter.mode(
-      //       context.mainTextColor,
-      //       BlendMode.srcIn,
-      //     ),
-      //   ),
-      // ),
-      // title: SearchTextField(),
-      // centerTitle: true,
-      // actions: [
-      //   Padding(
-      //     padding: EdgeInsets.symmetric(horizontal: 16),
-      //     child: IconButton(
-      //       onPressed: () {
-      //         showModalBottomSheet(
-      //             backgroundColor: context.scaffoldBackgroundColor,
-      //             context: context,
-      //             builder: (context) {
-      //               return SearchFilterBottomSheet();
-      //             });
-      //       },
-      //       icon: FilterIcon(),
-      //     ),
-      //   ),
-      // ],
     );
   }
 }
