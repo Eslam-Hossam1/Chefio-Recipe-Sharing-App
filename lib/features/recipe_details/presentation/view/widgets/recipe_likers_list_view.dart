1a34decf48701e3d40b28b8b13d1f2280c10a8e0
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-06-19 10:13:12 +0300

    nav on click recipe liker

--- lib/features/recipe_details/presentation/view/widgets/recipe_likers_list_view.dart
+++ lib/features/recipe_details/presentation/view/widgets/recipe_likers_list_view.dart
@@ -1,6 +1,8 @@
+import 'package:chefio_app/core/utils/routing/routing_helper.dart';
 import 'package:chefio_app/features/recipe_details/data/models/recipe_liker_model/recipe_liker_model.dart';
 import 'package:chefio_app/features/recipe_details/presentation/view/widgets/user_tile_item.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class RecipeLikersListView extends StatelessWidget {
   const RecipeLikersListView({
@@ -16,11 +18,20 @@ class RecipeLikersListView extends StatelessWidget {
       controller: scrollController,
       padding: EdgeInsets.only(top: 16),
       itemCount: recipeLikers.length,
-      itemBuilder: (context, index) => UserTileItem(
-        userName: recipeLikers[index].user.username,
-        baseSize: 50,
-        userImageUrl: recipeLikers[index].user.profilePicture,
-        intermediateSpace: 16,
+      itemBuilder: (context, index) => GestureDetector(
+        onTap: () {
+          context.push(
+            RoutingHelper.getProfilePath(
+              chefId: recipeLikers[index].id,
+            ),
+          );
+        },
+        child: UserTileItem(
+          userName: recipeLikers[index].user.username,
+          baseSize: 50,
+          userImageUrl: recipeLikers[index].user.profilePicture,
+          intermediateSpace: 16,
+        ),
       ),
       separatorBuilder: (context, index) => SizedBox(
         height: 24,
