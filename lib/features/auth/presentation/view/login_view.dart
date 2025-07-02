211e93c3995fa2f732e30d46be0a0250d19a2668
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-02 18:15:18 +0300

    modify the handling of user enter non verified email while login state

--- lib/features/auth/presentation/view/login_view.dart
+++ lib/features/auth/presentation/view/login_view.dart
@@ -1,15 +1,19 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:chefio_app/core/Functions/get_auth_padding.dart';
+import 'package:chefio_app/core/api/dio_consumer.dart';
 import 'package:chefio_app/core/utils/routing/app_router.dart';
 import 'package:chefio_app/core/utils/constants.dart';
 import 'package:chefio_app/core/utils/dialog_helper.dart';
+import 'package:chefio_app/core/utils/routing/routing_helper.dart';
 import 'package:chefio_app/core/utils/routing/routs.dart';
+import 'package:chefio_app/core/utils/service_locator.dart';
 import 'package:chefio_app/core/widgets/adaptive_layout_widget.dart';
 import 'package:chefio_app/core/widgets/custom_cicular_progress_indicator.dart';
 import 'package:chefio_app/core/widgets/tablet_form_container.dart';
 import 'package:chefio_app/features/auth/presentation/manager/log_in_cubit.dart/log_in_cubit.dart';
 import 'package:chefio_app/features/auth/presentation/view/widgets/login_view_body.dart';
 import 'package:chefio_app/features/auth/presentation/view/widgets/login_view_body_tablet.dart';
+import 'package:chefio_app/features/otp/data/models/sign_up_reason.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -30,7 +34,13 @@ class LoginView extends StatelessWidget {
             btnOkOnPress: () {},
           );
         } else if (state is LogInNeedVerification) {
-          context.go(RoutePaths.verificationCode, extra: state.email);
+          RoutingHelper.pushOtp(
+            context,
+            otpReason: SignUpReason(
+              apiConsumer: getIt<DioConsumer>(),
+              email: state.email,
+            ),
+          );
         } else if (state is LogInSuccess) {
           context.go(RoutePaths.home);
         }
