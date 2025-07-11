00050cabb479080030993f6fbf67e86d7883b657
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-11 18:39:08 +0300

    create edit step item cubit

---
+++ lib/features/edit_recipe/presentation/manager/edit_step_item_cubit/edit_step_item_cubit.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:bloc/bloc.dart';
+import 'package:chefio_app/core/helpers/cropped_image_picker_helper.dart';
+import 'package:equatable/equatable.dart';
+import 'package:image_picker/image_picker.dart';
+
+part 'edit_step_item_state.dart';
+
+class StepItemCubit extends Cubit<StepItemState> {
+  final CroppedImagePickerHelper _croppedImagePickerHelper;
+  
+  StepItemCubit(this._croppedImagePickerHelper) : super(StepInitial());
+  void init({required String? imageUrl}) {
+    if (imageUrl != null) {
+      emit(UrlImage(imageUrl: imageUrl));
+    } else {
+      emit(NoImage());
+    }
+  }
+
+  Future<void> pickImageForChanging({required ImageSource imageSource}) async {
+    final returnedImage = await _croppedImagePickerHelper
+        .pickSquareCroppedImage(imageSource: imageSource);
+    if (returnedImage == null) return;
+    var imageFile = File(returnedImage.path);
+      emit(NoImage());
+    emit(PickedChangeStepImage(stepImageFile: imageFile));
+  }
+
+  Future<void> pickImageForAdding({required ImageSource imageSource}) async {
+    final returnedImage = await _croppedImagePickerHelper
+        .pickSquareCroppedImage(imageSource: imageSource);
+    if (returnedImage == null) return;
+    var imageFile = File(returnedImage.path);
+    emit(PickedAddStepImage(stepImageFile: imageFile));
+  }
+
+  void removeImage() {
+    emit(NoImage());
+  }
+}
