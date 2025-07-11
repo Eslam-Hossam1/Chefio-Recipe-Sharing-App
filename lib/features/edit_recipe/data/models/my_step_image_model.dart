aff794342662f101d148e1ca38e3d904f0641562
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-11 17:55:08 +0300

    delete previous models from upload

--- lib/features/edit_recipe/data/models/my_step_image_model.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-class MyStepImageModel {
-   int stepIndex;
-   File stepImageFile;
-
-  MyStepImageModel({required this.stepIndex, required this.stepImageFile});
-}
--- lib/features/edit_recipe/data/models/step_image_model.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-class StepImageModel {
-  int stepIndex;
-  File imageFile;
-
-  StepImageModel({required this.stepIndex, required this.imageFile});
-}
