04038ec34feaf7cc22f342510a69cebb10f2576c
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-13 17:46:14 +0300

    just fix api interceptor file name

--- lib/core/api/dio_consumer.dart
+++ lib/core/api/dio_consumer.dart
@@ -1,5 +1,5 @@
 import 'package:chefio_app/core/api/api_consumer.dart';
-import 'package:chefio_app/core/api/api_interceptors.dart';
+import 'package:chefio_app/core/api/api_interceptor.dart';
 import 'package:chefio_app/core/api/end_ponits.dart';
 import 'package:chefio_app/core/helpers/auth_credentials_helper.dart';
 import 'package:chefio_app/core/utils/service_locator.dart';
