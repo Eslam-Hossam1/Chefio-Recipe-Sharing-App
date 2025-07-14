1db4a353ae3e559a31362764db89af391e7653ba
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-14 21:27:47 +0300

    refactor my profile pop up menu button

--- lib/features/profile/presentation/views/widgets/my_profile_pop_up_menu_button.dart
+++ lib/features/profile/presentation/views/widgets/my_profile_pop_up_menu_button.dart
@@ -1,21 +1,41 @@
 import 'package:chefio_app/core/helpers/share_helper.dart';
+import 'package:chefio_app/core/models/pop_up_menu_model.dart';
 import 'package:chefio_app/core/utils/Localization/app_localization_keys/app_localization_keys.dart';
 import 'package:chefio_app/core/utils/routing/routs.dart';
 import 'package:chefio_app/core/utils/service_locator.dart';
-import 'package:chefio_app/core/utils/styles.dart';
 import 'package:chefio_app/core/utils/theme/theme_colors_extension.dart';
+import 'package:chefio_app/core/widgets/build_pop_up_menu_item.dart';
 import 'package:chefio_app/features/profile/presentation/manager/my_profile_cubit/my_profile_cubit.dart';
 import 'package:chefio_app/features/profile/presentation/manager/profile_cubit/profile_cubit.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 
 class MyProfilePopUpMenuButton extends StatelessWidget {
-  const MyProfilePopUpMenuButton({
-    super.key,
-  });
+  const MyProfilePopUpMenuButton({super.key});
+
+  static final List<PopUpMenuModel> popUpModels = [
+    PopUpMenuModel(
+      iconData: Icons.share,
+      textLocalizationKey: AppLocalizationKeys.profile.shareProfile,
+      onTap: (context) async {
+        await getIt<ShareHelper>().shareProfile(
+          chefId: context.read<ProfileCubit>().profileModel!.id,
+        );
+      },
+    ),
+    PopUpMenuModel(
+      iconData: Icons.edit,
+      textLocalizationKey: AppLocalizationKeys.profile.editProfile,
+      onTap: (context) {
+        context.push(
+          RoutePaths.editProfile,
+          extra: context.read<MyProfileCubit>().profileModel,
+        );
+      },
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -25,48 +45,13 @@ class MyProfilePopUpMenuButton extends StatelessWidget {
         FontAwesomeIcons.ellipsisVertical,
         color: context.mainTextColor,
       ),
-      itemBuilder: (context) {
-        return [
-          PopupMenuItem(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.share, color: context.mainTextColor),
-                Text(
-                  AppLocalizationKeys.profile.shareProfile.tr(),
-                  style: Styles.textStyleMedium15(context).copyWith(
-                    color: context.mainTextColor,
-                  ),
-                ),
-              ],
-            ),
-            onTap: () async {
-              await getIt<ShareHelper>().shareProfile(
-                chefId: context.read<ProfileCubit>().profileModel!.id,
-              );
-            },
-          ),
-          PopupMenuItem(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.edit, color: context.mainTextColor),
-                  Text(
-                    AppLocalizationKeys.profile.editProfile.tr(),
-                    style: Styles.textStyleMedium15(context).copyWith(
-                      color: context.mainTextColor,
-                    ),
-                  ),
-                ],
-              ),
-              onTap: () {
-                context.push(
-                  RoutePaths.editProfile,
-                  extra: context.read<MyProfileCubit>().profileModel,
-                );
-              }),
-        ];
-      },
+      itemBuilder: (context) => List.generate(
+        popUpModels.length,
+        (index) => buildPopUpMenuItem(
+          context,
+          popUpMenuModel: popUpModels[index],
+        ),
+      ),
     );
   }
 }
