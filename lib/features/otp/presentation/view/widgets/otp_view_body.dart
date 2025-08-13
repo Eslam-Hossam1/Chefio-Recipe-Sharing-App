e83aab3df5ec69ecd83a407e6df376b1ebc01c0f
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-13 05:53:55 +0300

    style(otp): add wrap OtpViewBody widget with adaptive padding widget

--- lib/features/otp/presentation/view/widgets/otp_view_body.dart
+++ lib/features/otp/presentation/view/widgets/otp_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:chefio_app/core/Localization/app_localization_keys/app_localization_keys.dart';
 import 'package:chefio_app/core/theme/styles.dart';
 import 'package:chefio_app/core/theme/theme_colors_extension.dart';
+import 'package:chefio_app/core/widgets/adaptive_padding.dart';
 import 'package:chefio_app/features/otp/presentation/view/widgets/otp_form.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
@@ -10,42 +11,44 @@ class OtpViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(slivers: [
-      SliverFillRemaining(
-        hasScrollBody: false,
-        child: Column(
-          children: [
-            Expanded(
-                child: SizedBox(
-              height: 20,
-            )),
-            Text(
-              AppLocalizationKeys.auth.verificationCodeViewCheckYourEmail.tr(),
-              style: Styles.textStyleBold22(context).copyWith(
-                color: context.mainTextColor,
+    return AdaptivePadding(
+      child: CustomScrollView(slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Column(
+            children: [
+              Expanded(
+                  child: SizedBox(
+                height: 20,
+              )),
+              Text(
+                AppLocalizationKeys.auth.verificationCodeViewCheckYourEmail.tr(),
+                style: Styles.textStyleBold22(context).copyWith(
+                  color: context.mainTextColor,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Text(
-              AppLocalizationKeys.auth.verificationCodeViewWeHaveSent.tr(),
-              style: Styles.textStyleMedium15(context).copyWith(
-                color: context.secondaryTextColor,
+              SizedBox(
+                height: 8,
               ),
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            OtpForm(),
-            Expanded(
-                flex: 2,
-                child: SizedBox(
-                  height: 20,
-                )),
-          ],
+              Text(
+                AppLocalizationKeys.auth.verificationCodeViewWeHaveSent.tr(),
+                style: Styles.textStyleMedium15(context).copyWith(
+                  color: context.secondaryTextColor,
+                ),
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              OtpForm(),
+              Expanded(
+                  flex: 2,
+                  child: SizedBox(
+                    height: 20,
+                  )),
+            ],
+          ),
         ),
-      ),
-    ]);
+      ]),
+    );
   }
 }
