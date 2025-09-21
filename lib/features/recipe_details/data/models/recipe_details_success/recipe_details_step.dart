9c12588648beade97827cabdf5bc347febd04886
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-21 18:11:06 +0300

    feat: make id field in RecipeDetailsStep model nullable
    - the api sometimes give me the id and sometimes not so make it nullable ensures no unexpected errors will happed
    - it not used so it safe to make it nullable

--- lib/features/recipe_details/data/models/recipe_details_success/recipe_details_step.dart
+++ lib/features/recipe_details/data/models/recipe_details_success/recipe_details_step.dart
@@ -1,7 +1,7 @@
 class RecipeDetailsStep {
   String step;
   String? stepImage;
-  String id;
+  String? id;
 
   RecipeDetailsStep({required this.step, this.stepImage, required this.id});
 
@@ -9,7 +9,7 @@ class RecipeDetailsStep {
       RecipeDetailsStep(
         step: json['step'] as String,
         stepImage: json['stepImage'] as String?,
-        id: json['_id'] as String,
+        id: json['_id'] as String?,
       );
 
   Map<String, dynamic> toJson() => {
