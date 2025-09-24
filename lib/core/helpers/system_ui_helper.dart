1cd14dacde2b851e1dcdfef45f9a0aae403860da
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-24 20:40:48 +0300

    feat(core): create SystemUIHelper class
    - to handle change system navigation bar color while changing theme

---
+++ lib/core/helpers/system_ui_helper.dart
@@ -0,0 +1,55 @@
+import 'package:chefio_app/core/theme/app_colors.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
+abstract class SystemUIHelper {
+  static void setSystemUIForTheme(
+    BuildContext context,
+    ThemeMode mode,
+  ) {
+    switch (mode) {
+      case ThemeMode.system:
+        _setSystemUIForSystemTheme(context);
+        break;
+      case ThemeMode.light:
+        _setSystemUIForLightTheme();
+        break;
+      case ThemeMode.dark:
+        _setSystemUIForDarkTheme();
+        break;
+    }
+  }
+
+  static _setSystemUIForLightTheme() {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        systemNavigationBarColor: AppColors.lightScaffoldBackground,
+        systemNavigationBarIconBrightness: Brightness.light,
+        systemNavigationBarDividerColor: Colors.black,
+      ),
+    );
+  }
+
+  static _setSystemUIForDarkTheme() {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        systemNavigationBarColor: AppColors.darkScaffoldBackground,
+        systemNavigationBarIconBrightness: Brightness.dark,
+        systemNavigationBarDividerColor: Colors.white,
+      ),
+    );
+  }
+
+  static _setSystemUIForSystemTheme(BuildContext context) {
+    final Brightness platformBrightness =
+        MediaQuery.of(context).platformBrightness;
+    switch (platformBrightness) {
+      case Brightness.dark:
+        _setSystemUIForDarkTheme();
+        break;
+      case Brightness.light:
+        _setSystemUIForLightTheme();
+        break;
+    }
+  }
+}
