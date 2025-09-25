db3bc723bf570af7fdfc316414866d01c45c0f39
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-25 06:20:44 +0300

    fix(edit-profile): add localization to EditSubmitButton

--- lib/features/edit_profile/presentation/views/widgets/edit_submit_button.dart
+++ lib/features/edit_profile/presentation/views/widgets/edit_submit_button.dart
@@ -1,5 +1,7 @@
+import 'package:chefio_app/core/Localization/app_localization_keys/app_localization_keys.dart';
 import 'package:chefio_app/core/theme/styles.dart';
 import 'package:chefio_app/core/widgets/custom_text_button.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class EditSubmitButton extends StatelessWidget {
@@ -14,7 +16,7 @@ class EditSubmitButton extends StatelessWidget {
       onPressed: onPressed,
       width: double.infinity,
       child: Text(
-        "Submit",
+       AppLocalizationKeys.global.submit.tr(),
         style: Styles.textStyleBold15(context).copyWith(color: Colors.white),
       ),
     );
